1e5b89e15e28f4531c9c480669939051e30ca356
xl-xueling <better_xueling@126.com>
2023-08-13 09:54:16 +0800

    init

--- lighthouse-core/src/main/java/com/dtstep/lighthouse/core/builtin/BuiltinLoader.java
+++ lighthouse-core/src/main/java/com/dtstep/lighthouse/core/builtin/BuiltinLoader.java
@@ -145,6 +145,7 @@ public final class BuiltinLoader {
                 columnList.add(metaColumn);
             }
             Map<String,ColumnTypeEnum> columnHashMap = columnList.stream().collect(Collectors.toMap(MetaColumn::getColumnName, MetaColumn::getColumnTypeEnum));
+            groupExtEntity.setAllRelatedColumns(columnHashMap);
             groupExtEntity.setRunningRelatedColumns(columnHashMap);
             groupExtEntity.setColumnList(columnList);
             groupExtEntity.setState(GroupStateEnum.RUNNING.getState());
