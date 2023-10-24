d8554ea406a8137c8c2cc162e70d890dcd419efe
xl-xueling <better_xueling@126.com>
2023-10-24 09:47:03 +0800

    init lighthouse-insights module.

---
+++ lighthouse-insights/src/main/resources/local/config-overrides.js
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const {
+  override,
+  addWebpackModuleRule,
+  addWebpackPlugin,
+  addWebpackAlias,
+} = require('customize-cra');
+const ArcoWebpackPlugin = require('@arco-plugins/webpack-react');
+const addLessLoader = require('customize-cra-less-loader');
+const setting = require('./src/settings.json');
+
+module.exports = {
+  webpack: override(
+    addLessLoader({
+      lessLoaderOptions: {
+        lessOptions: {},
+      },
+    }),
+    addWebpackModuleRule({
+      test: /\.svg$/,
+      loader: '@svgr/webpack',
+    }),
+    addWebpackPlugin(
+      new ArcoWebpackPlugin({
+        theme: '@arco-themes/react-arco-pro',
+        modifyVars: {
+          'arcoblue-6': setting.themeColor,
+        },
+      })
+    ),
+    addWebpackAlias({
+      '@': path.resolve(__dirname, 'src'),
+    })
+  ),
+};
