adf3ff0e9e1d453f6df225c85881c827df597dee
xl-xueling <better_xueling@126.com>
2023-11-09 17:10:25 +0800

    init lighthouse-insights module

--- lighthouse-insights/src/main/resources/light-insights/src/mock/user.ts
+++ lighthouse-insights/src/main/resources/light-insights/src/mock/user.ts
@@ -33,28 +33,36 @@ if (!isSSR) {
       });
 
       // 登录
-      Mock.mock(new RegExp('/api/user/login'), (params) => {
+      Mock.mock(new RegExp('/api/v1/user/login'), (params) => {
         const { userName, password } = JSON.parse(params.body);
         if (!userName) {
           return {
-            status: 'error',
-            msg: '用户名不能为空',
+            code:'1',
+            msg:'用户名不能为空',
+            data:{
+            }
           };
         }
         if (!password) {
           return {
-            status: 'error',
-            msg: '密码不能为空',
+            code:'2',
+            msg:'密码不能为空',
+            data:{}
           };
         }
         if (userName === 'admin' && password === 'admin') {
           return {
-            status: 'ok',
+            code:'0',
+            msg:'登录成功！',
+            data:{
+              token:"sasucessawwxoks"
+            }
           };
         }
         return {
-          status: 'error',
-          msg: '账号或者密码错误',
+          code:'3',
+          msg:'账号密码错误！',
+          data:{}
         };
       });
     },
