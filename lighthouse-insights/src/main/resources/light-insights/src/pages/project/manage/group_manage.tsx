21fbe835e85fcb7abd4da49b73830ae80bcb9c72
xl-xueling <better_xueling@126.com>
2023-11-20 17:10:17 +0800

    feat:user module optimization.

--- lighthouse-insights/src/main/resources/light-insights/src/pages/project/manage/group_manage.tsx
+++ lighthouse-insights/src/main/resources/light-insights/src/pages/project/manage/group_manage.tsx
@@ -2,14 +2,10 @@ import { Button, Card, Radio, Tabs } from '@arco-design/web-react';
 import React from 'react';
 import useLocale from '@/utils/useLocale';
 import locale from './locale';
-import DataStatisticList from './statistic-list';
 import styles from './style/index.module.less';
+import QuickOperation from "./statistic-list";
 
-import AceEditor from 'react-ace';
-import 'ace-builds/src-noconflict/ace'
-import 'ace-builds/src-noconflict/theme-monokai';// monokai的主题样式
-import 'ace-builds/src-noconflict/ext-language_tools'; // 代码联想
-import 'ace-builds/src-noconflict/mode-xml';
+
 
 export default function GroupManage() {
   const t = useLocale(locale);
@@ -21,18 +17,10 @@ export default function GroupManage() {
             <Button type="text">{}</Button>
             <Button>{'创建'}</Button>
           </div>
-          {/*<div className={styles['data-statistic-list-content']}>*/}
-          {/*  <DataStatisticList />*/}
-          {/*</div>*/}
+          <div className={styles['data-statistic-list-content']}>
+            <QuickOperation />
+          </div>
           <div>
-            <AceEditor
-                mode="xml"
-                theme="monokai"
-                enableBasicAutocompletion={true}
-                wrapEnabled={true}
-                name="UNIQUE_ID_OF_DIV"
-                editorProps={{ $blockScrolling: true }}
-            />
           </div>
         </div>
       </div>
