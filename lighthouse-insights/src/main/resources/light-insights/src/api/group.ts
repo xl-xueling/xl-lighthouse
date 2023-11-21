aadbda223ff97a711f85b6832b2fe7ed345ee1b4
xl-xueling <better_xueling@126.com>
2023-11-21 18:28:08 +0800

    feat:insights stat module optimization.

--- lighthouse-insights/src/main/resources/light-insights/src/api/group.ts
+++ lighthouse-insights/src/main/resources/light-insights/src/api/group.ts
@@ -1,5 +1,24 @@
 import {request} from "@/utils/request";
 import {ArcoTreeNode, ResultData} from "@/types/insights-common";
-import {Project, User} from "@/types/insights-web";
+import {Group, Project, User} from "@/types/insights-web";
+
+export async function requestQueryById(id:number) :Promise<ResultData<Group>> {
+    return request({
+        url:'/group/queryById',
+        method:'POST',
+        id,
+    })
+}
+
+export async function requestDeleteByID(id:number) :Promise<ResultData> {
+    return request({
+        url:'/group/deleteById',
+        method:'POST',
+        id,
+    })
+}
+
+
+
 
 
