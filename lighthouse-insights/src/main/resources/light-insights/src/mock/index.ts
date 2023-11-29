de41312a2380dbfee930460a2d7d8e6f362daa7d
xl-xueling <better_xueling@126.com>
2023-11-29 16:10:29 +0800

    feat:insights project module optimization.

--- lighthouse-insights/src/main/resources/light-insights/src/mock/index.ts
+++ lighthouse-insights/src/main/resources/light-insights/src/mock/index.ts
@@ -1,7 +1,6 @@
 import Mock from 'mockjs';
 import { isSSR } from '@/utils/is';
 
-import './user';
 import './message-box';
 import '../pages/department/mock'
 import '../pages/user/mock'
--- lighthouse-insights/src/main/resources/light-insights/src/pages/user/mock/index.ts
+++ lighthouse-insights/src/main/resources/light-insights/src/pages/user/mock/index.ts
@@ -148,6 +148,40 @@ setupMock({
         });
 
 
+        Mock.mock(new RegExp('/api/v1/user/login'), (params) => {
+            const { userName, password } = JSON.parse(params.body);
+            if (!userName) {
+                return {
+                    code:'1',
+                    message:'用户名不能为空',
+                    data:{
+                    }
+                };
+            }
+            if (!password) {
+                return {
+                    code:'2',
+                    message:'密码不能为空',
+                    data:{}
+                };
+            }
+            if (userName === 'admin' && password === 'admin') {
+                return {
+                    code:'0',
+                    message:'登录成功！',
+                    data:{
+                        token:"sasucessawwxoks"
+                    }
+                };
+            }
+            return {
+                code:'3',
+                message:'账号密码错误！',
+                data:{}
+            };
+        });
+
+
 
         const generatePermission = (role: string) => {
             const actions = role === 'admin' ? ['*'] : ['read'];
