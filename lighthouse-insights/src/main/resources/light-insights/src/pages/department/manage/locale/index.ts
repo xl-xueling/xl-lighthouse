b5150ed7db469b05802a1ff051c39d049f74260a
xl-xueling <better_xueling@126.com>
2023-11-29 11:57:28 +0800

    feat:insights project module optimization.

--- lighthouse-insights/src/main/resources/light-insights/src/pages/department/manage/locale/index.ts
+++ lighthouse-insights/src/main/resources/light-insights/src/pages/department/manage/locale/index.ts
@@ -2,10 +2,14 @@ const i18n = {
   'en-US': {
     'department.enterprise.structure': 'Enterprise Structure',
     'department.manage.limitExceeded':'Department name character limit exceeded',
+    'department.manage.deletePrompt':'Are you sure to delete this item?',
+    'department.manage.deleteHasChild':'The node has child,delete child-node first!',
   },
   'zh-CN': {
     'department.enterprise.structure': '组织架构',
     'department.manage.limitExceeded':'部门名称超出限制',
+    'department.manage.deletePrompt':'是否确认删除当前节点？',
+    'department.manage.deleteHasChild':'请删除子节点后再执行该操作!',
   },
 };
 
