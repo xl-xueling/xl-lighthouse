b700d83bb3a3a4d8941fea3e93e464f2982e9d3e
xl-xueling <better_xueling@126.com>
2023-12-01 12:34:44 +0800

    feat:insights project module optimization.

---
+++ lighthouse-insights/src/main/resources/light-insights/src/pages/project/create/locale/index.ts
@@ -0,0 +1,20 @@
+const i18n = {
+  'en-US': {
+    'projectCreate.form.title':'Create Project',
+    'projectCreate.form.name.errMsg':'Project name cannot be empty',
+    'projectCreate.form.name.less.limit':'Project name length is less than required',
+    'projectCreate.form.name.exceeds.limit':'Project name length exceeds limit',
+    'projectCreate.form.department.errMsg':'Department cannot be empty',
+    'projectCreate.form.description.errMsg':'Description cannot be empty',
+  },
+  'zh-CN': {
+    'projectCreate.form.title':'创建工程',
+    'projectCreate.form.name.errMsg':'工程名称不能为空',
+    'projectCreate.form.name.less.limit':'工程名称低于长度要求',
+    'projectCreate.form.name.exceeds.limit':'工程名称长度超出限制',
+    'projectCreate.form.department.errMsg':'请选择部门',
+    'projectCreate.form.description.errMsg':'描述信息不能为空',
+  },
+};
+
+export default i18n;
