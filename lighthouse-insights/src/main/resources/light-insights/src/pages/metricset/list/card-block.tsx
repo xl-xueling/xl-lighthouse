79c32df9015e30a6b1036edc9f5d1c9448285f5e
xl-xueling <better_xueling@126.com>
2023-12-07 20:40:40 +0800

    feat:insights project module optimization.

--- lighthouse-insights/src/main/resources/light-insights/src/pages/metricset/list/card-block.tsx
+++ lighthouse-insights/src/main/resources/light-insights/src/pages/metricset/list/card-block.tsx
@@ -7,30 +7,25 @@ import {
   Card,
   Descriptions,
   Typography,
-  Dropdown,
-  Menu,
   Skeleton,
 } from '@arco-design/web-react';
 import {
-  IconStarFill,
-  IconThumbUpFill,
-  IconSunFill,
-  IconFaceSmileFill,
-  IconPenFill,
-  IconCheckCircleFill,
-  IconCloseCircleFill,
-  IconMore, IconThumbUp, IconShareInternal,
+    IconStarFill,
+    IconThumbUpFill,
+    IconSunFill,
+    IconFaceSmileFill,
+    IconPenFill,
+    IconMore, IconThumbUp, IconShareInternal, IconPushpin, IconDelete, IconEdit,
 } from '@arco-design/web-react/icon';
-import PermissionWrapper from '@/components/PermissionWrapper';
 import useLocale from '@/utils/useLocale';
 import locale from './locale';
-import { QualityInspection, BasicCard } from './interface';
 import styles from './style/index.module.less';
+import {MetricSet} from "@/types/insights-web";
 
 interface CardBlockType {
-  type: 'quality' | 'service' | 'rules';
-  card: QualityInspection & BasicCard;
+  item: MetricSet;
   loading?: boolean;
+  callback;
 }
 
 const IconList = [
@@ -44,9 +39,8 @@ const IconList = [
 const { Paragraph } = Typography;
 
 function CardBlock(props: CardBlockType) {
-  const { type, card = {} } = props;
+  const {item ,callback } = props;
   const [visible, setVisible] = useState(false);
-  const [status, setStatus] = useState(card.status);
   const [loading, setLoading] = useState(props.loading);
 
   const t = useLocale(locale);
@@ -54,7 +48,6 @@ function CardBlock(props: CardBlockType) {
     setLoading(true);
     await new Promise((resolve) =>
       setTimeout(() => {
-        setStatus(status !== 1 ? 1 : 0);
         resolve(null);
       }, 1000)
     ).finally(() => setLoading(false));
@@ -64,142 +57,34 @@ function CardBlock(props: CardBlockType) {
     setLoading(props.loading);
   }, [props.loading]);
 
-  useEffect(() => {
-    if (card.status !== status) {
-      setStatus(card.status);
-    }
-  }, [card.status]);
-
-  const getTitleIcon = () => {
-    if (type === 'service' && typeof card.icon === 'number') {
-      return (
-        <div className={styles.icon}>
-          {IconList[card.icon % IconList.length]}
-        </div>
-      );
-    }
-    return null;
-  };
-
-  const getButtonGroup = () => {
-    if (type === 'quality') {
-      return (
-        <>
-          <PermissionWrapper
-            requiredPermissions={[
-              { resource: /^menu.list.*/, actions: ['read'] },
-            ]}
-          >
-            <Button
-              type="primary"
-              style={{ marginLeft: '12px' }}
-              loading={loading}
-            >
-              {t['cardList.options.qualityInspection']}
-            </Button>
-          </PermissionWrapper>
-
-          <PermissionWrapper
-            requiredPermissions={[
-              { resource: /^menu.list.*/, actions: ['write'] },
-            ]}
-          >
-            <Button loading={loading}>{t['cardList.options.remove']}</Button>
-          </PermissionWrapper>
-        </>
-      );
-    }
-
-    if (type === 'service') {
-      return (
-        <>
-          {status === 1 ? (
-            <Button loading={loading} onClick={changeStatus}>
-              {t['cardList.options.cancel']}
-            </Button>
-          ) : (
-            <Button type="outline" loading={loading} onClick={changeStatus}>
-              {status === 0
-                ? t['cardList.options.subscribe']
-                : t['cardList.options.renewal']}
-            </Button>
-          )}
-        </>
-      );
-    }
-
-    return (
-      <Switch checked={!!status} loading={loading} onChange={changeStatus} />
-    );
-  };
-
-  const getStatus = () => {
-    if (type === 'rules' && status) {
-      return (
-        <Tag
-          color="green"
-          icon={<IconCheckCircleFill />}
-          className={styles.status}
-          size="small"
-        >
-          {t['cardList.tag.activated']}
-        </Tag>
-      );
-    }
-    switch (status) {
-      case 1:
-        return (
-          <Tag
-            color="green"
-            icon={<IconCheckCircleFill />}
-            className={styles.status}
-            size="small"
-          >
-            {t['cardList.tag.opened']}
-          </Tag>
-        );
-      case 2:
-        return (
-          <Tag
-            color="red"
-            icon={<IconCloseCircleFill />}
-            className={styles.status}
-            size="small"
-          >
-            {t['cardList.tag.expired']}
-          </Tag>
-        );
-      default:
-        return null;
-    }
-  };
 
   const getContent = () => {
     if (loading) {
       return (
         <Skeleton
-          text={{ rows: type !== 'quality' ? 3 : 2 }}
+          text={{ rows: 3 }}
           animation
           className={styles['card-block-skeleton']}
         />
       );
     }
-    if (type !== 'quality') {
-      return <Paragraph>{card.description}</Paragraph>;
-    }
+    // if (type !== 'quality') {
+    //   return <Paragraph>{item.description}</Paragraph>;
+    // }
     return (
       <Descriptions
         column={2}
+        colon=' :'
         data={[
-          { label: '待质检数', value: card.qualityCount },
-          { label: '积压时长', value: `${card.duration}s` },
-          { label: '待抽检数', value: card.randomCount },
+          { label: '总点击数', value: "--" },
+          { label: '总点赞数', value: "--" },
         ]}
       />
     );
+
   };
 
-  const className = cs(styles['card-block'], styles[`${type}-card`]);
+  const className = cs(styles['card-block']);
 
   return (
     <Card
@@ -207,16 +92,20 @@ function CardBlock(props: CardBlockType) {
       className={className}
       size="small"
       actions={[
-        <span key={1} className='icon-hover'>
-          <IconThumbUp />
+        <span key={1} className='icon-hover' onClick={() => callback('like',item.id)}>
+          <IconThumbUp/>
         </span>,
-        <span key={2} className='icon-hover'>
+        <span key={2} className='icon-hover' onClick={() => callback('share',item.id)}>
           <IconShareInternal />
         </span>,
-        <span key={3} className='icon-hover'>
-          <IconMore />
+          <span key={3} className='icon-hover' onClick={() => callback('update',item.id)}>
+          <IconEdit />
+        </span>,
+        <span key={4} className='icon-hover' onClick={() => callback('delete',item.id)}>
+          <IconDelete />
         </span>,
       ]}
+
       title={
         loading ? (
           <Skeleton
@@ -232,33 +121,18 @@ function CardBlock(props: CardBlockType) {
                 [styles['title-more']]: visible,
               })}
             >
-              {getTitleIcon()}
-              {card.title}
-              {getStatus()}
-              <Dropdown
-                droplist={
-                  <Menu>
-                    {['操作1', '操作2'].map((item, key) => (
-                      <Menu.Item key={key.toString()}>{item}</Menu.Item>
-                    ))}
-                  </Menu>
-                }
-                trigger="click"
-                onVisibleChange={setVisible}
-                popupVisible={visible}
-              >
-                <div className={styles.more}>
-                  <IconMore />
-                </div>
-              </Dropdown>
+              {/*{getTitleIcon()}*/}
+              {item.title}
+              <div className={styles.more} onClick={() => callback('fixed',item.id)}>
+                <IconPushpin />
+              </div>
             </div>
-            <div className={styles.time}>{card.time}</div>
+            <div className={styles.time}>{item.createdTime}</div>
           </>
         )
       }
     >
       <div className={styles.content}>{getContent()}</div>
-      <div className={styles.extra}>{getButtonGroup()}</div>
     </Card>
   );
 }
