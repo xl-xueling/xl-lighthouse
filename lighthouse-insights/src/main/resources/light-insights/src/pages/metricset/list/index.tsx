05d5f5d7a62db598959c5592c44022e5dd99dd12
xl-xueling <better_xueling@126.com>
2023-12-07 15:02:50 +0800

    feat:insights project module optimization.

--- lighthouse-insights/src/main/resources/light-insights/src/pages/metricset/list/index.tsx
+++ lighthouse-insights/src/main/resources/light-insights/src/pages/metricset/list/index.tsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import {Tabs, Card, Input, Typography, Grid, Space, Avatar} from '@arco-design/web-react';
+import {Tabs, Card, Input, Typography, Grid, Space, Avatar, PaginationProps} from '@arco-design/web-react';
 import useLocale from '@/utils/useLocale';
 import locale from './locale';
 import styles from './style/index.module.less';
 import CardBlock from './card-block';
 import AddCard from './card-add';
 import { QualityInspection, BasicCard } from './interface';
-import './mock';
-import {IconMore, IconShareInternal, IconThumbUp} from "@arco-design/web-react/icon";
 import MetricSetAddPanel from "@/pages/metricset/create";
+import {MetricSet, Project} from "@/types/insights-web";
+import {requestList} from "@/api/metricset";
 
 const { Title } = Typography;
 const { Row, Col } = Grid;
@@ -19,23 +19,62 @@ export default function ListCard() {
   const t = useLocale(locale);
   const [loading, setLoading] = useState(true);
   const [showCreatePanel,setShowCreatePanel] = useState<boolean>(false);
-  const [data, setData] = useState({
-    quality: defaultList,
-    service: defaultList,
-    rules: defaultList,
-  });
+  // const [data, setData] = useState({
+  //   quality: defaultList,
+  //   service: defaultList,
+  //   rules: defaultList,
+  // });
+  const [listData,setListData] = useState<{quality:MetricSet[]}>({quality:[]});
 
   const [activeKey, setActiveKey] = useState('all');
     const { Meta } = Card;
 
-  const getData = () => {
-    axios
-      .get('/api/cardList')
-      .then((res) => {
-        setData(res.data);
+    const [pagination, setPagination] = useState<PaginationProps>({
+        sizeOptions: [15,20,30,50],
+        sizeCanChange: true,
+        showTotal: true,
+        pageSize: 15,
+        current: 1,
+        pageSizeChangeResetCurrent: true,
+    });
+    const [formParams, setFormParams] = useState({});
+
+  // const getData = () => {
+  //   axios
+  //     .get('/api/cardList')
+  //     .then((res) => {
+  //       setData(res.data);
+  //     })
+  //     .finally(() => setLoading(false));
+  // };
+
+  const fetchData = async () => {
+      setLoading(true);
+      const {current, pageSize} = pagination;
+      const fetchMetricSetsInfo:Promise<{list:Array<MetricSet>,total:number}> = new Promise<{list:Array<MetricSet>,total:number}>((resolve) => {
+          const proc = async () => {
+              const result = await requestList({
+                  params: {
+                      page: current,
+                      pageSize,
+                      ...formParams,
+                  },
+              });
+              resolve(result.data);
+          }
+          proc().then();
       })
-      .finally(() => setLoading(false));
-  };
+
+      const result = await Promise.all([fetchMetricSetsInfo]);
+      const {list,total}:{list:Array<MetricSet>,total:number} = result[0];
+      console.log("result is:" + JSON.stringify(result));
+      setListData({"quality":list});
+      setLoading(false);
+  }
+
+    useEffect(() => {
+        fetchData().then();
+    }, []);
 
   const handleShowCreatePanel = () => {
       setShowCreatePanel(true);
@@ -46,13 +85,10 @@ export default function ListCard() {
   }
 
 
-  useEffect(() => {
-    getData();
-  }, []);
-  7;
+
   const getCardList = (
     list: Array<BasicCard & QualityInspection>,
-    type: keyof typeof data
+    type: keyof typeof listData
   ) => {
     return (
       <Row gutter={24} className={styles['card-content']}>
@@ -76,6 +112,7 @@ export default function ListCard() {
       <Tabs
         activeTab={activeKey}
         type="rounded"
+        size={"small"}
         onChange={setActiveKey}
         extra={
           <Input.Search
@@ -84,24 +121,18 @@ export default function ListCard() {
           />
         }
       >
-        <Tabs.TabPane key="all" title={t['cardList.tab.title.all']} />
-        <Tabs.TabPane key="quality" title={t['cardList.tab.title.quality']} />
-        <Tabs.TabPane key="service" title={t['cardList.tab.title.service']} />
-        <Tabs.TabPane key="rules" title={t['cardList.tab.title.rules']} />
+        <Tabs.TabPane key="all" title={t['metricSetList.tab.title.all']} />
+        <Tabs.TabPane key="quality" title={t['metricSetList.tab.title.owner']} />
       </Tabs>
       <div className={styles.container}>
-        {activeKey === 'all' ? (
-          Object.entries(data).map(([key, list]) => (
-            <div key={key}>
-              <Title heading={6}>{t[`cardList.tab.title.${key}`]}</Title>
-              {getCardList(list, key as keyof typeof data)}
-            </div>
-          ))
-        ) : (
-          <div className={styles['single-content']}>
-            {getCardList(data[activeKey], activeKey as keyof typeof data)}
-          </div>
-        )}
+          {
+              Object.entries(listData).map(([key, list]) => (
+                  <div key={key}>
+                      <Title heading={6}>{t[`cardList.tab.title.${key}`]}</Title>
+                      {getCardList(list, key as keyof typeof listData)}
+                  </div>
+              ))
+          }
       </div>
 
         {showCreatePanel && <MetricSetAddPanel onClose={handleHideCreatePanel}/>}
--- lighthouse-insights/src/main/resources/light-insights/src/pages/metricset/mock/index.ts
+++ lighthouse-insights/src/main/resources/light-insights/src/pages/metricset/mock/index.ts
@@ -2,6 +2,34 @@ import Mock from 'mockjs';
 import setupMock from '@/utils/setupMock';
 import {Project} from "@/types/insights-web";
 
+const list  = Mock.mock([
+        {
+            id: 101,
+            'title': '@word() @word() @word()',
+            'description': '@sentence()',
+            "createdTime":'@datetime',
+        },
+        {
+            id: 102,
+            'title': '@word() @word() @word()',
+            'description': '@sentence()',
+            "createdTime":'@datetime',
+        },
+        {
+            id: 103,
+            'title': '@word() @word() @word()',
+            'description': '@sentence()',
+            "createdTime":'@datetime',
+        },
+        {
+            id: 104,
+            'title': '@word() @word() @word()',
+            'description': '@sentence()',
+            "createdTime":'@datetime',
+        },
+    ]);
+
+
 setupMock({
 
     setup: () => {
@@ -12,6 +40,18 @@ setupMock({
                 message:'success',
                 data:{
                     id:1,
+                },
+            };
+        });
+
+        Mock.mock(new RegExp('/api/v1/metricset/list'), (params) => {
+            console.log("receive create params,params:" + JSON.stringify(params));
+            return {
+                code:'0',
+                message:'success',
+                data:{
+                    list: list,
+                    total: 100,
                 },
             };
         });
