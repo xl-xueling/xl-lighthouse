25b79f080d28328218d5752ab75e103796cc3692
xl-xueling <better_xueling@126.com>
2023-12-10 14:24:59 +0800

    feat:insights project module optimization.

--- lighthouse-insights/src/main/resources/light-insights/src/pages/stat/display/index.tsx
+++ lighthouse-insights/src/main/resources/light-insights/src/pages/stat/display/index.tsx
@@ -1,15 +1,17 @@
 import React, {useEffect} from 'react';
 import {useParams} from "react-router-dom";
 import FiltersForm from "./filters_form";
-import {Card, Grid} from "@arco-design/web-react";
+import {Card, Divider, Grid, Typography} from "@arco-design/web-react";
 import styles from "./style/index.module.less";
 import {useSelector} from "react-redux";
 import {Department} from "@/types/insights-web";
 import useLocale from "@/utils/useLocale";
 import locale from "@/pages/project/list/locale";
+import ChartPanel from "@/pages/stat/display/chart_panel";
+import {IconDashboard} from "@arco-design/web-react/icon";
 const { Row, Col } = Grid;
 
-export default function StatDisplay({statId}) {
+export default function StatDisplay({statId = 0}) {
 
     const allDepartInfo = useSelector((state: {allDepartInfo:Array<Department>}) => state.allDepartInfo);
     const t = useLocale(locale);
@@ -20,10 +22,17 @@ export default function StatDisplay({statId}) {
     },[])
 
     return(
-        <Card>
+        <Card style={{width:'90%'}}>
+            <Typography.Title
+                style={{  fontSize:15}}
+            >
+                <IconDashboard style={{fontSize:17}}/> {'每分钟uv数据统计'}
+            </Typography.Title>
+            <Divider/>
             <div className={styles['search-form-wrapper']}>
                 <FiltersForm />
             </div>
+            <ChartPanel />
         </Card>
     );
 }
