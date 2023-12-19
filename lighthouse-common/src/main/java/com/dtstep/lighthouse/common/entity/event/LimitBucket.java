3ae279e9d0c6531b7c3f7a514de34f00b75523b4
xl-xueling <better_xueling@126.com>
2023-12-19 14:51:11 +0800

    feat:insights project module optimization.

--- lighthouse-common/src/main/java/com/dtstep/lighthouse/common/entity/event/LimitBucket.java
+++ lighthouse-common/src/main/java/com/dtstep/lighthouse/common/entity/event/LimitBucket.java
@@ -2,7 +2,7 @@ package com.dtstep.lighthouse.common.entity.event;
 
 import com.dtstep.lighthouse.common.entity.calculate.MicroBucket;
 /*
- * Copyright (C) 2022-2023 XueLing.雪灵
+ * Copyright (C) 2022-2024 XueLing.雪灵
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
