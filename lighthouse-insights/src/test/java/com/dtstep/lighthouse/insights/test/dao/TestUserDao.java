09091a246a9e5e00ca85d7c315f03ef0c4cb9801
xl-xueling <better_xueling@126.com>
2023-12-19 16:33:55 +0800

    feat:insights module project init.

---
+++ lighthouse-insights/src/test/java/com/dtstep/lighthouse/insights/test/dao/TestUserDao.java
@@ -0,0 +1,24 @@
+package com.dtstep.lighthouse.insights.test.dao;
+
+import com.dtstep.lighthouse.insights.LightHouseInsightsApplication;
+import com.dtstep.lighthouse.insights.dao.UserMapper;
+import com.dtstep.lighthouse.insights.modal.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = LightHouseInsightsApplication.class,properties = {"spring.config.location=classpath:lighthouse-insights.yml"})
+public class TestUserDao {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void testCreateUser() throws Exception {
+        userMapper.insert(new User());
+    }
+}
