fa10e13d81ada91fb5fbc0f56ace67fccf0f0a18
xl-xueling <better_xueling@126.com>
2023-12-20 16:39:10 +0800

    feat:insights project module init.

--- lighthouse-insights/src/main/java/com/dtstep/lighthouse/insights/dao/ProjectDao.java
+++ lighthouse-insights/src/main/java/com/dtstep/lighthouse/insights/dao/ProjectDao.java
@@ -1,9 +1,13 @@
 package com.dtstep.lighthouse.insights.dao;
 
+import com.dtstep.lighthouse.insights.dto.ProjectQueryParam;
 import com.dtstep.lighthouse.insights.modal.Project;
+
+import java.util.List;
 
 public interface ProjectDao {
 
     int insert(Project project);
 
+    List<Project> queryList(ProjectQueryParam queryParam,Integer pageNum,Integer pageSize);
 }
---
+++ lighthouse-insights/src/main/java/com/dtstep/lighthouse/insights/dto/ProjectQueryParam.java
@@ -0,0 +1,66 @@
+package com.dtstep.lighthouse.insights.dto;
+
+import java.util.Date;
+
+public class ProjectQueryParam {
+
+    private Integer id;
+
+    private String search;
+
+    private Integer departmentId;
+
+    private Date createStartTime;
+
+    private Date createEndTime;
+
+    private Integer owner;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getSearch() {
+        return search;
+    }
+
+    public void setSearch(String search) {
+        this.search = search;
+    }
+
+    public Integer getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Integer departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public Date getCreateStartTime() {
+        return createStartTime;
+    }
+
+    public void setCreateStartTime(Date createStartTime) {
+        this.createStartTime = createStartTime;
+    }
+
+    public Date getCreateEndTime() {
+        return createEndTime;
+    }
+
+    public void setCreateEndTime(Date createEndTime) {
+        this.createEndTime = createEndTime;
+    }
+
+    public Integer getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Integer owner) {
+        this.owner = owner;
+    }
+}
