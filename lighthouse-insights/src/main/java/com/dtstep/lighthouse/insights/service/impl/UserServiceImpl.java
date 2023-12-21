d651b221191e6fb01c8b997e06214425943622bc
xl-xueling <better_xueling@126.com>
2023-12-21 14:50:54 +0800

    feat:insights project module init.

--- lighthouse-insights/src/main/java/com/dtstep/lighthouse/insights/service/impl/UserServiceImpl.java
+++ lighthouse-insights/src/main/java/com/dtstep/lighthouse/insights/service/impl/UserServiceImpl.java
@@ -1,7 +1,9 @@
 package com.dtstep.lighthouse.insights.service.impl;
 
+import com.dtstep.lighthouse.common.enums.user.UserStateEnum;
 import com.dtstep.lighthouse.commonv2.entity.user.Role;
 import com.dtstep.lighthouse.commonv2.enums.AuthRoleTypeEnum;
+import com.dtstep.lighthouse.insights.dao.DepartmentDao;
 import com.dtstep.lighthouse.insights.dao.UserDao;
 import com.dtstep.lighthouse.insights.modal.User;
 import com.dtstep.lighthouse.insights.service.UserService;
@@ -9,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -17,9 +20,26 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserDao userDao;
 
+    @Autowired
+    private DepartmentDao departmentDao;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
     @Override
     public int create(User user) {
-        return userDao.insert(user);
+        boolean isExist = userDao.isUserNameExist(user.getUsername());
+        if(isExist){
+            return -1;
+        }else{
+            Date date = new Date();
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            user.setState(UserStateEnum.USER_PEND);
+            user.setUpdateTime(date);
+            user.setLastTime(date);
+            user.setCreateTime(date);
+            return userDao.insert(user);
+        }
     }
 
     @Override
