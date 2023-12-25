f1320d518e314e3acd3b7d9e600ca14d882b438b
xl-xueling <better_xueling@126.com>
2023-12-25 11:03:17 +0800

    feat:insights project module init.

---
+++ lighthouse-insights/src/main/java/com/dtstep/lighthouse/insights/types/JsonObjectTypeHandler.java
@@ -0,0 +1,63 @@
+package com.dtstep.lighthouse.insights.types;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+
+import java.io.IOException;
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class JsonObjectTypeHandler<T> extends BaseTypeHandler<T> {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    private TypeReference<T> typeReference = null;
+
+    public JsonObjectTypeHandler(TypeReference<T> typeReference) {
+        this.typeReference = typeReference;
+    }
+
+    @Override
+    public void setNonNullParameter(PreparedStatement ps, int i, T parameter, JdbcType jdbcType) throws SQLException {
+        try {
+            String json = objectMapper.writeValueAsString(parameter);
+            ps.setString(i, json);
+        } catch (JsonProcessingException e) {
+            throw new SQLException("Failed to convert Map to JSON", e);
+        }
+    }
+
+    @Override
+    public T getNullableResult(ResultSet rs, String columnName) throws SQLException {
+        String json = rs.getString(columnName);
+        return parseJson(json);
+    }
+
+    @Override
+    public T getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+        String json = rs.getString(columnIndex);
+        return parseJson(json);
+    }
+
+    @Override
+    public T getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+        String json = cs.getString(columnIndex);
+        return parseJson(json);
+    }
+
+    private T parseJson(String json) throws SQLException {
+        try {
+            if (json == null) {
+                return null;
+            }
+            return objectMapper.readValue(json, typeReference);
+        } catch (IOException e) {
+            throw new SQLException("Failed to parse JSON", e);
+        }
+    }
+}
