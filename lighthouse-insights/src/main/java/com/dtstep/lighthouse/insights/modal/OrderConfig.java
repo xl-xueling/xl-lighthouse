2e44bd901e865cec1549658533de576cddf5bfd8
xl-xueling <better_xueling@126.com>
2023-12-28 22:06:14 +0800

    feat:insights project module init.

---
+++ lighthouse-insights/src/main/java/com/dtstep/lighthouse/insights/modal/OrderConfig.java
@@ -0,0 +1,16 @@
+package com.dtstep.lighthouse.insights.modal;
+
+import java.util.List;
+
+public class OrderConfig {
+
+    private List<String> steps;
+
+    public List<String> getSteps() {
+        return steps;
+    }
+
+    public void setSteps(List<String> steps) {
+        this.steps = steps;
+    }
+}
