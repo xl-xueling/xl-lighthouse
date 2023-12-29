bc9ec96cf3afbbf8e89d67faa82683b853395a92
xl-xueling <better_xueling@126.com>
2023-12-29 21:10:25 +0800

    feat:insights project module init.

---
+++ lighthouse-insights/src/main/java/com/dtstep/lighthouse/insights/dto/OrderQueryParam.java
@@ -0,0 +1,24 @@
+package com.dtstep.lighthouse.insights.dto;
+
+public class OrderQueryParam {
+
+    private Integer applyUserId;
+
+    private Integer approveUserId;
+
+    public Integer getApplyUserId() {
+        return applyUserId;
+    }
+
+    public void setApplyUserId(Integer applyUserId) {
+        this.applyUserId = applyUserId;
+    }
+
+    public Integer getApproveUserId() {
+        return approveUserId;
+    }
+
+    public void setApproveUserId(Integer approveUserId) {
+        this.approveUserId = approveUserId;
+    }
+}
--- lighthouse-insights/src/main/java/com/dtstep/lighthouse/insights/service/OrderService.java
+++ lighthouse-insights/src/main/java/com/dtstep/lighthouse/insights/service/OrderService.java
@@ -1,8 +1,16 @@
 package com.dtstep.lighthouse.insights.service;
 
+import com.dtstep.lighthouse.commonv2.insights.ListData;
+import com.dtstep.lighthouse.insights.dto.OrderQueryParam;
 import com.dtstep.lighthouse.insights.modal.Order;
+
+import java.util.List;
 
 public interface OrderService {
 
     int create(Order order);
+
+    ListData<Order> queryApplyList(OrderQueryParam queryParam,Integer pageNum,Integer pageSize);
+
+    ListData<Order> queryApproveList(OrderQueryParam queryParam,Integer pageNum,Integer pageSize);
 }
