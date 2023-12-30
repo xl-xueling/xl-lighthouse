030a2d981ffc0c9f4220c072c911f7f23adbca24
xl-xueling <better_xueling@126.com>
2023-12-30 22:13:13 +0800

    feat:insights project module init.

--- lighthouse-insights/src/main/resources/light-insights/src/pages/order/approve/list/process.tsx
+++ lighthouse-insights/src/main/resources/light-insights/src/pages/order/approve/list/process.tsx
@@ -2,7 +2,7 @@ import React, {useEffect, useMemo, useState} from 'react';
 import {Modal} from "@arco-design/web-react";
 import OrderDetail from "@/pages/order/common/detail";
 
-export default function OrderProcessPanel({onClose}) {
+export default function OrderProcessPanel({orderInfo,onClose}) {
 
     return(
         <Modal
@@ -10,7 +10,7 @@ export default function OrderProcessPanel({onClose}) {
             style={{ width:'850px',top:'20px' }}
             visible={true}
             onCancel={onClose}>
-            <OrderDetail />
+            <OrderDetail orderInfo={orderInfo} />
         </Modal>
     );
 }
