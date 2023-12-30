5b0e42af08426158f54843e1deb3e6fdb0a2618e
xl-xueling <better_xueling@126.com>
2023-12-30 19:04:21 +0800

    feat:insights project module init.

--- lighthouse-insights/src/main/java/com/dtstep/lighthouse/insights/dao/PermissionDao.java
+++ lighthouse-insights/src/main/java/com/dtstep/lighthouse/insights/dao/PermissionDao.java
@@ -1,8 +1,13 @@
 package com.dtstep.lighthouse.insights.dao;
 
+import com.dtstep.lighthouse.insights.dto.PermissionQueryParam;
 import com.dtstep.lighthouse.insights.modal.Permission;
+
+import java.util.List;
 
 public interface PermissionDao {
 
     int insert(Permission permission);
+
+    List<Permission> queryList(PermissionQueryParam queryParam,Integer pageNum,Integer pageSize);
 }
---
+++ lighthouse-insights/src/main/java/com/dtstep/lighthouse/insights/dto/PermissionQueryParam.java
@@ -0,0 +1,24 @@
+package com.dtstep.lighthouse.insights.dto;
+
+public class PermissionQueryParam {
+
+    private Integer roleId;
+
+    private Integer ownerType;
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public Integer getOwnerType() {
+        return ownerType;
+    }
+
+    public void setOwnerType(Integer ownerType) {
+        this.ownerType = ownerType;
+    }
+}
