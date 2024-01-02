2cb1e2917469633263d23930ca814d52a6ecba2d
xl-xueling <better_xueling@126.com>
2024-01-02 18:56:36 +0800

    feat:insights project module init.

--- lighthouse-insights/src/main/java/com/dtstep/lighthouse/insights/service/impl/RoleServiceImpl.java
+++ lighthouse-insights/src/main/java/com/dtstep/lighthouse/insights/service/impl/RoleServiceImpl.java
@@ -4,10 +4,12 @@ import com.dtstep.lighthouse.insights.dao.RoleDao;
 import com.dtstep.lighthouse.insights.enums.RoleTypeEnum;
 import com.dtstep.lighthouse.insights.modal.Role;
 import com.dtstep.lighthouse.insights.service.RoleService;
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class RoleServiceImpl implements RoleService {
@@ -17,12 +19,12 @@ public class RoleServiceImpl implements RoleService {
 
     @Override
     public void initRole() {
-        if(!isRoleExist(RoleTypeEnum.FULL_MANAGE_PERMISSION,0)){
-            Role fullManageRole = new Role(RoleTypeEnum.FULL_MANAGE_PERMISSION,0);
+        if(!isRoleExist(RoleTypeEnum.DEPARTMENT_MANAGE_PERMISSION,0)){
+            Role fullManageRole = new Role(RoleTypeEnum.DEPARTMENT_MANAGE_PERMISSION,0);
             create(fullManageRole);
         }
-        if(!isRoleExist(RoleTypeEnum.FULL_ACCESS_PERMISSION,0)){
-            Role fullAccessRole = new Role(RoleTypeEnum.FULL_ACCESS_PERMISSION,0);
+        if(!isRoleExist(RoleTypeEnum.DEPARTMENT_ACCESS_PERMISSION,0)){
+            Role fullAccessRole = new Role(RoleTypeEnum.DEPARTMENT_ACCESS_PERMISSION,0);
             create(fullAccessRole);
         }
         if(!isRoleExist(RoleTypeEnum.OPT_MANAGE_PERMISSION,0)){
@@ -42,6 +44,19 @@ public class RoleServiceImpl implements RoleService {
         role.setCreateTime(localDateTime);
         role.setUpdateTime(localDateTime);
         return roleDao.insert(role);
+    }
+
+    @Override
+    public void batchCreate(List<Role> list) {
+        if(CollectionUtils.isEmpty(list)){
+            return;
+        }
+        LocalDateTime localDateTime = LocalDateTime.now();
+        list.forEach(z -> {
+            z.setCreateTime(localDateTime);
+            z.setUpdateTime(localDateTime);
+        });
+        roleDao.batchInsert(list);
     }
 
     @Override
