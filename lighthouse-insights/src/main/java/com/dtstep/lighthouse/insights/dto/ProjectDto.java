6e24218f6186e2005917b44d07212c99e5598b35
xl-xueling <better_xueling@126.com>
2024-01-06 09:52:04 +0800

    feat:insights project module init.

--- lighthouse-insights/src/main/java/com/dtstep/lighthouse/insights/dto/ProjectDto.java
+++ lighthouse-insights/src/main/java/com/dtstep/lighthouse/insights/dto/ProjectDto.java
@@ -7,6 +7,7 @@ import com.dtstep.lighthouse.insights.modal.Department;
 import com.dtstep.lighthouse.insights.modal.Project;
 import com.dtstep.lighthouse.insights.modal.User;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProjectDto extends Project {
@@ -14,6 +15,22 @@ public class ProjectDto extends Project {
     private List<User> admins;
 
     private List<CommonTreeNode> structure;
+
+    private List<PermissionInfo.PermissionEnum> permissions = new ArrayList<>();
+
+    public List<PermissionInfo.PermissionEnum> getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(List<PermissionInfo.PermissionEnum> permissions) {
+        this.permissions = permissions;
+    }
+
+    public void addPermission(PermissionInfo.PermissionEnum permissionEnum){
+        if(permissionEnum != null && !this.permissions.contains(permissionEnum)){
+            this.permissions.add(permissionEnum);
+        }
+    }
 
     public ProjectDto(Project project){
         assert project != null;
