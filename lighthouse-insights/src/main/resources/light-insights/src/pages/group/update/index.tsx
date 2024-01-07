0fafb0c84b26bffe6f8db9a12a225f5097a0fcdd
xl-xueling <better_xueling@126.com>
2024-01-07 22:35:15 +0800

    feat:insights project module init.

--- lighthouse-insights/src/main/resources/light-insights/src/pages/group/update/index.tsx
+++ lighthouse-insights/src/main/resources/light-insights/src/pages/group/update/index.tsx
@@ -208,7 +208,7 @@ export default function GroupUpdatePanel({groupInfo,onClose}) {
                             </Typography.Title>
                         </Grid.Col>
                         <Grid.Col span={8} style={{ textAlign: 'right' }}>
-                            <Button type={"secondary"} size={"mini"} onClick={() => editTableRef.current.addRow()}>添加</Button>
+                            <Button type={"secondary"} size={"mini"} onClick={() => editTableRef.current.addRow()}>{t['groupUpdate.form.button.columnAdd']}</Button>
                         </Grid.Col>
                     </Grid.Row>
                     <EditTable ref={editTableRef} columnsProps={columnsProps} columnsData={initData}/>
