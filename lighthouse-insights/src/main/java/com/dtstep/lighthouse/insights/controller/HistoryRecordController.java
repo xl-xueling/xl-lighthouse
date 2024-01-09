e0d47bb8e1ec071d9979381c46c4492b7a198c94
xl-xueling <better_xueling@126.com>
2024-01-09 09:57:33 +0800

    feat:insights project module init.

---
+++ lighthouse-insights/src/main/java/com/dtstep/lighthouse/insights/controller/HistoryRecordController.java
@@ -0,0 +1,10 @@
+package com.dtstep.lighthouse.insights.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ControllerAdvice
+public class HistoryRecordController {
+
+}
