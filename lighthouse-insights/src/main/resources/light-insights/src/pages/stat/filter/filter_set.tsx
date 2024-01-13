40041701eec81bcfe2b72ba1c9b19f118df0171a
xl-xueling <better_xueling@126.com>
2024-01-13 13:47:25 +0800

    feat:insights project module init.

--- lighthouse-insights/src/main/resources/light-insights/src/pages/stat/filter/filter_set.tsx
+++ lighthouse-insights/src/main/resources/light-insights/src/pages/stat/filter/filter_set.tsx
@@ -22,7 +22,10 @@ import {Component, ComponentTypeEnum, RenderFilterConfig} from "@/types/insights
 import {translateToTreeNodes} from "@/pages/department/common";
 import {requestList} from "@/api/component";
 import SystemComponents from "@/pages/stat/filter/system_component";
-
+import {FormInstance} from "@arco-design/web-react/lib";
+const EditableContext = React.createContext<{ getForm?: () => FormInstance }>({});
+const FormItem = Form.Item;
+import styles from "./style/index.module.less";
 
 export default function StatFilterConfigModal({onClose}) {
 
@@ -40,7 +43,7 @@ export default function StatFilterConfigModal({onClose}) {
             render: (value, record) => {
                 if(value == ComponentTypeEnum.FILTER_INPUT){
                     return (
-                        <Input size={"small"}/>
+                        <Input size={"mini"}/>
                     )
                 }else{
                     return (
@@ -163,7 +166,11 @@ export default function StatFilterConfigModal({onClose}) {
     ]
 
     useEffect(() => {
-        setSourceData(defaultComponents);
+        if(defaultComponents){
+            setSourceData(defaultComponents.map(z => {
+                return {...z, key: getRandomString()};
+            }));
+        }
     },[])
 
     const changeComponentType = (componentType) => {
@@ -182,17 +189,17 @@ export default function StatFilterConfigModal({onClose}) {
     }
 
     function handleSave(row) {
-        // const newData = [...targetData];
-        // const index = newData.findIndex((item) => row.key === item.key);
-        // newData.splice(index, 1, { ...newData[index], ...row });
-        // setTargetData(newData);
-        console.log("-")
+        const newData = [...targetData];
+        const index = newData.findIndex((item) => row.key === item.key);
+        newData.splice(index, 1, { ...newData[index], ...row });
+        setTargetData(newData);
     }
 
     return (
         <Modal
+            className={styles['edit-cell']}
             title= '修改筛选项'
-            style={{ width:'1000px',top:'20px',maxWidth:'70%' }}
+            style={{ width:'1000px',top:'20px',maxWidth:'70%',minHeight:'600px' }}
             visible={true}
             onCancel={() => onClose()}>
             <Space size={10} direction="vertical" style={{width:'100%'}}>
@@ -208,9 +215,137 @@ export default function StatFilterConfigModal({onClose}) {
                 <Typography.Title style={{fontSize:'14px',marginTop:'20px'}}>
                     {'当前配置：'}
                 </Typography.Title>
-                <EditTable ref={editTableRef} columnsProps={targetColumns} columnsData={targetData}/>
+                {/*<EditTable ref={editTableRef} columnsProps={targetColumns} columnsData={targetData}/>*/}
+
+                <Table size={"mini"} className={'editable-cell'}
+                       style={{minHeight:'200px'}}
+                       data={targetData}
+                       components={{
+                           body: {
+                               row: EditableRow,
+                               cell: EditableCell,
+                           },
+                       }}
+                       columns={targetColumns.map((column) => {
+                               return column.editable
+                                   ? {
+                                       ...column,
+                                       onCell: () => ({
+                                           onHandleSave: handleSave,
+                                       }),
+                                   }
+                                   : column
+                           }
+
+                       )}
+                />
             </Space>
         </Modal>
+    );
+}
+
+
+function EditableRow(props) {
+    const { children, record, className, ...rest } = props;
+    const refForm = useRef(null);
+
+    const getForm = () => refForm.current;
+
+    return (
+        <EditableContext.Provider
+            value={{
+                getForm,
+            }}
+        >
+            <Form
+                style={{ display: 'table-row' }}
+                ref={refForm}
+                wrapper='tr'
+                wrapperProps={rest}
+                className={`${className} editable-row`}
+            >
+                {props.children}
+            </Form>
+        </EditableContext.Provider>
+    );
+}
+
+function EditableCell(props) {
+    const { children, className, rowData, column, onHandleSave } = props;
+    const ref = useRef(null);
+    const refInput = useRef(null);
+    const { getForm } = useContext(EditableContext);
+    const [editing, setEditing] = useState(false);
+    const handleClick = useCallback(
+        (e) => {
+            if (
+                editing &&
+                column.editable &&
+                ref.current &&
+                !ref.current.contains(e.target)
+            ) {
+                cellValueChangeHandler(rowData[column.dataIndex]);
+            }
+        },
+        [editing, rowData, column]
+    );
+    useEffect(() => {
+        if(column.componentType == EditTableComponentEnum.INPUT){
+            editing && refInput.current.focus();
+        }
+    }, [editing]);
+
+    useEffect(() => {
+        document.addEventListener('click', handleClick, true);
+        return () => {
+            document.removeEventListener('click', handleClick, true);
+        };
+    }, [handleClick]);
+
+    const cellValueChangeHandler = (value) => {
+        console.log("---value is:" + value);
+        value = "sss";
+        if (column.componentType == EditTableComponentEnum.SELECT) {
+            const values = {
+                [column.dataIndex]: value,
+            };
+            onHandleSave && onHandleSave({ ...rowData, ...values });
+            setTimeout(() => setEditing(!editing), 300);
+        } else {
+            const form = getForm();
+            form.validate([column.dataIndex], (errors, values) => {
+                if (!errors || !errors[column.dataIndex]) {
+                    setEditing(!editing);
+                    onHandleSave && onHandleSave({ ...rowData, ...values });
+                }
+            });
+        }
+    };
+
+    if (column.componentType == EditTableComponentEnum.INPUT && editing) {
+        console.log("show cell input...")
+        return (
+            <div ref={ref}>
+                <FormItem
+                    style={{ marginBottom: 0 }}
+                    labelCol={{ span: 0 }}
+                    wrapperCol={{ span: 24 }}
+                    initialValue={rowData[column.dataIndex]}
+                    field={column.dataIndex}>
+                    <Input size={"mini"} ref={refInput} onPressEnter={cellValueChangeHandler} />
+                </FormItem>
+            </div>
+        );
+    }
+    return (
+        <div
+            className={column.editable ? `editable-cell ${className}` : className}
+            onClick={() => {
+                column.editable && setEditing(!editing)
+            }}
+        >
+            {children}
+        </div>
     );
 }
 
