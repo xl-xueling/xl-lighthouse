b5c3a8b8935447827563da2419d401a67141f29b
xl-xueling <better_xueling@126.com>
2024-01-14 13:23:36 +0800

    feat:insights project module init.

---
+++ lighthouse-insights/src/main/java/com/dtstep/lighthouse/insights/dto/MetricSetQueryParam.java
@@ -0,0 +1,24 @@
+package com.dtstep.lighthouse.insights.dto;
+
+public class MetricSetQueryParam {
+
+    private Integer id;
+
+    private String title;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
--- lighthouse-insights/src/main/java/com/dtstep/lighthouse/insights/service/MetricService.java
+++
@@ -1,6 +0,0 @@
-package com.dtstep.lighthouse.insights.service;
-
-public interface MetricService {
-
-
-}
