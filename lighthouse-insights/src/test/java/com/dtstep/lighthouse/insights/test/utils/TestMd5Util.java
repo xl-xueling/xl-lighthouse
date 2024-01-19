af2b89da4886d390b51c12e70a85411f279e7a07
xl-xueling <better_xueling@126.com>
2024-01-19 10:19:11 +0800

    feat:insights project module init.

---
+++ lighthouse-insights/src/test/java/com/dtstep/lighthouse/insights/test/utils/TestMd5Util.java
@@ -0,0 +1,22 @@
+package com.dtstep.lighthouse.insights.test.utils;
+
+import com.dtstep.lighthouse.common.entity.group.GroupVerifyEntity;
+import com.dtstep.lighthouse.common.lru.Cache;
+import com.dtstep.lighthouse.common.lru.LRU;
+import com.dtstep.lighthouse.common.util.Md5Util;
+import com.dtstep.lighthouse.core.batch.BatchAdapter;
+import org.junit.Test;
+
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+public class TestMd5Util {
+
+    private static final Cache<String, String> cacheHolder = LRU.newBuilder().maximumSize(5000).expireAfterWrite(2, TimeUnit.MINUTES).softValues().build();
+
+    @Test
+    public void test() throws Exception {
+
+    }
+}
