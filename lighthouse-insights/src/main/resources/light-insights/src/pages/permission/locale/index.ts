dd8da63ee7dc34d0850bf548ea070edb7c35c0a5
xl-xueling <better_xueling@126.com>
2024-01-22 19:04:13 +0800

    feat:insights project module init.

--- lighthouse-insights/src/main/resources/light-insights/src/pages/permission/locale/index.ts
+++ lighthouse-insights/src/main/resources/light-insights/src/pages/permission/locale/index.ts
@@ -1,9 +1,19 @@
 const i18n = {
     'en-US': {
-
+        'permissionManage.modal.title':'Permission Manage',
+        'permissionManage.department.accessPermission':'Department Access Permission',
+        'permissionManage.user.accessPermission':'User Access Permission',
+        'permissionManage.user.managePermission':'User Manage Permission',
+        'permissionManage.user.grantPermission':'Grant Permission',
+        'permissionManage.grant.submit':'Submit',
     },
     'zh-CN': {
-
+        'permissionManage.modal.title':'权限管理',
+        'permissionManage.department.accessPermission':'部门访问权限',
+        'permissionManage.user.accessPermission':'用户访问权限',
+        'permissionManage.user.managePermission':'用户访问权限',
+        'permissionManage.user.grantPermission':'添加权限',
+        'permissionManage.grant.submit':'提交修改',
     },
 };
 
