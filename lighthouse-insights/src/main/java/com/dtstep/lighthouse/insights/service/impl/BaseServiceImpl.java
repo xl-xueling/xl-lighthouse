d3b6d8bc323162ab5263a8beb42aeabcf7c69037
xl-xueling <better_xueling@126.com>
2024-01-24 13:50:57 +0800

    feat:insights project module init.

--- lighthouse-insights/src/main/java/com/dtstep/lighthouse/insights/service/impl/BaseServiceImpl.java
+++ lighthouse-insights/src/main/java/com/dtstep/lighthouse/insights/service/impl/BaseServiceImpl.java
@@ -21,27 +21,4 @@ public class BaseServiceImpl implements BaseService {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         return authentication == null ? -1 : (Integer) authentication.getPrincipal();
     }
-
-    @Deprecated
-    @Override
-    public <T> ListData<T> translateToListData(List<T> list) {
-        if(CollectionUtils.isEmpty(list)){
-            PageInfo<T> pageInfo = new PageInfo<>(list);
-            ListData<T> listData = new ListData<>();
-            listData.setList(Lists.newArrayList());
-            listData.setTotal(0);
-            listData.setPageNum(0);
-            listData.setPageSize(0);
-            return listData;
-        }else{
-            PageInfo<T> pageInfo = new PageInfo<>(list);
-            ListData<T> listData = new ListData<>();
-            listData.setList(pageInfo.getList());
-            listData.setTotal(pageInfo.getTotal());
-            listData.setPageNum(pageInfo.getPageNum());
-            listData.setPageSize(pageInfo.getPageSize());
-            return listData;
-        }
-    }
-
 }
