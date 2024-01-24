bec76ed43975c86502dab105d097b497d8573cb5
xl-xueling <better_xueling@126.com>
2024-01-24 09:59:54 +0800

    feat:insights project module init.

--- lighthouse-insights/src/test/java/com/dtstep/lighthouse/insights/test/utils/TestAviator.java
+++ lighthouse-insights/src/test/java/com/dtstep/lighthouse/insights/test/utils/TestAviator.java
@@ -1,14 +1,25 @@
 package com.dtstep.lighthouse.insights.test.utils;
 
+import com.dtstep.lighthouse.common.entity.state.StatState;
 import com.dtstep.lighthouse.core.expression.embed.AviatorHandler;
+import com.dtstep.lighthouse.core.formula.FormulaCalculate;
+import com.dtstep.lighthouse.core.formula.FormulaTranslate;
+import org.apache.commons.lang3.tuple.Pair;
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 public class TestAviator {
 
     @Test
     public void testFormula() throws Exception {
-        String formula = "ass";
-        Object obj = AviatorHandler.execute(formula);
-        System.out.println(obj);
+        String formula = "userId";
+        Map<String,Object> envMap = new HashMap<>();
+        envMap.put("userId", UUID.randomUUID().toString());
+        Object obj = AviatorHandler.execute(formula,envMap);
+        System.out.println("obj:" + obj);
     }
 }
