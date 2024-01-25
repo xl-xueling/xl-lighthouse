34fcc6bdc3c96c96db32b31128c2df9a63c03aac
xl-xueling <better_xueling@126.com>
2024-01-25 21:59:24 +0800

    feat:insights project module init.

--- lighthouse-insights/src/main/resources/light-insights/src/pages/order/apply/modal/ApplyModal.tsx
+++ lighthouse-insights/src/main/resources/light-insights/src/pages/order/apply/modal/ApplyModal.tsx
@@ -1,19 +1,12 @@
 import {
     Typography,
     Grid,
-    Button,
-    Form,
-    Input,
-    Tabs,
-    Dropdown, Menu, TreeSelect, Card, Table, TableColumnProps, Space, Divider, PaginationProps, Notification
 } from '@arco-design/web-react';
-import {
-    IconDownCircle, IconPlus, IconTag, IconThunderbolt
-} from '@arco-design/web-react/icon';
 import React, {useEffect, useMemo, useState} from 'react';
 
 import ProjectApplyModal from "@/pages/project/apply";
 import {ResourceTypeEnum} from "@/types/insights-common";
+import StatApplyModal from "@/pages/stat/apply";
 const { Row, Col } = Grid;
 const { Text } = Typography;
 
@@ -23,10 +16,8 @@ export default function ApplyModal({itemInfo,resourceType,onClose}) {
     const getRenderModal = () => {
         if(resourceType == ResourceTypeEnum.Project){
             return <ProjectApplyModal projectInfo={itemInfo} onClose={onClose}/>
-        }else if(resourceType == ResourceTypeEnum.Metric){
-            return null;
         }else if(resourceType == ResourceTypeEnum.Stat){
-
+            return <StatApplyModal statInfo={itemInfo} onClose={onClose}/>
         }
         return null;
     }
