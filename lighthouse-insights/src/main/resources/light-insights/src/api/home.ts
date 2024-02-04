d17520a567d08d08374fd69141ce8925fd2e720e
xl-xueling <better_xueling@126.com>
2024-02-04 17:33:10 +0800

    feat:insights project module init.

--- lighthouse-insights/src/main/resources/light-insights/src/api/home.ts
+++ lighthouse-insights/src/main/resources/light-insights/src/api/home.ts
@@ -1,8 +1,8 @@
 import {request} from "@/utils/request";
 import {Component, ResultData} from "@/types/insights-common";
-import {Department, Order, Project} from "@/types/insights-web";
+import {Department, HomeData, Order, Project} from "@/types/insights-web";
 
-export async function requestOverView() :Promise<ResultData<Record<number,Component>>> {
+export async function requestOverView() :Promise<ResultData<HomeData>> {
     return request({
         url:'/homepage/overview',
         method:'POST',
