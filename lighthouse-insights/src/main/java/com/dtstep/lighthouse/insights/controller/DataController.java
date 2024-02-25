c7af411bf769867d92fdf5461ae945a941cc8302
xl-xueling <better_xueling@126.com>
2024-02-25 15:17:00 +0800

    feat:insights project module init.

--- lighthouse-insights/src/main/java/com/dtstep/lighthouse/insights/controller/DataController.java
+++ lighthouse-insights/src/main/java/com/dtstep/lighthouse/insights/controller/DataController.java
@@ -1,5 +1,11 @@
 package com.dtstep.lighthouse.insights.controller;
 
+import com.dtstep.lighthouse.common.enums.RoleTypeEnum;
+import com.dtstep.lighthouse.common.modal.Role;
+import com.dtstep.lighthouse.commonv2.insights.ResultCode;
+import com.dtstep.lighthouse.insights.service.BaseService;
+import com.dtstep.lighthouse.insights.service.PermissionService;
+import com.dtstep.lighthouse.insights.service.RoleService;
 import com.dtstep.lighthouse.insights.vo.ResultData;
 import com.dtstep.lighthouse.common.modal.StatDataObject;
 import com.dtstep.lighthouse.common.util.JsonUtil;
@@ -21,6 +27,15 @@ public class DataController {
     @Autowired
     private DataService dataService;
 
+    @Autowired
+    private RoleService roleService;
+
+    @Autowired
+    private PermissionService permissionService;
+
+    @Autowired
+    private BaseService baseService;
+
     @PostMapping("/data/stat")
     public ResultData<List<StatDataObject>> dataQuery(@Validated @RequestBody DataStatQueryParam queryParam){
         System.out.println("query param is:" + JsonUtil.toJSONString(queryParam));
@@ -30,6 +45,13 @@ public class DataController {
 
     @PostMapping("/test-data/stat")
     public ResultData<List<StatDataObject>> testDataQuery(@Validated @RequestBody DataStatQueryParam queryParam){
+        int statId = queryParam.getStatId();
+        int userId = baseService.getCurrentUserId();
+        Role role = roleService.queryRole(RoleTypeEnum.STAT_ACCESS_PERMISSION,statId);
+        boolean hasPermission = permissionService.checkUserPermission(userId,role.getId());
+        if(!hasPermission){
+            return ResultData.result(ResultCode.accessDenied);
+        }
         List<StatDataObject> objectList = dataService.testDataQuery(queryParam.getStatId(),queryParam.getStartTime(),queryParam.getEndTime(),null);
         System.out.println("objectList is:" + JsonUtil.toJSONString(objectList));
         return ResultData.success(objectList);
