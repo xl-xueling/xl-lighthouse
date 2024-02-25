50cde175538e484006ef67a47325d977143baaf2
xl-xueling <better_xueling@126.com>
2024-02-25 13:45:43 +0800

    feat:insights project module init.

--- lighthouse-insights/src/main/resources/light-insights/src/pages/metricset/common/locale/index.ts
+++ lighthouse-insights/src/main/resources/light-insights/src/pages/metricset/common/locale/index.ts
@@ -1,9 +1,17 @@
 const i18n = {
   'en-US': {
     'MetricNavModal.modal.title':'MetricSet Shortcuts',
+    'metricStar.operations.star.submit.success':'MetricSet has been stared!',
+    'metricStar.operations.star.confirm':'Are you sure to star this item?',
+    'metricStar.operations.unstar.confirm':'Are you sure to unstar this item?',
+    'metricStar.operations.unstar.submit.success':'MetricSet has been unStared!'
   },
   'zh-CN': {
     'MetricNavModal.modal.title':'指标集导航',
+    'metricStar.operations.star.submit.success':'已关注当前指标集！',
+    'metricStar.operations.star.confirm':'是否确认关注当前指标集',
+    'metricStar.operations.unstar.confirm':'是否确认取消关注当前指标集？',
+    'metricStar.operations.unstar.submit.success':'指标集已被取消关注！',
   },
 };
 
