030581e9ef8dcd51ab49a066a6297d553554dc93
xl-xueling <better_xueling@126.com>
2024-02-27 21:42:17 +0800

    feat:insights project module init.

--- lighthouse-insights/src/main/resources/light-insights/src/pages/metricset/common/index.tsx
+++
@@ -1,6 +0,0 @@
-import React, {useCallback, useEffect, useMemo, useState} from 'react';
-
-export default function Index() {
-
-    return (<div>ss</div>);
-}
--- lighthouse-insights/src/main/resources/light-insights/src/pages/order/index.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useEffect } from 'react';
-import {ResultData} from "@/types/insights-common";
-
-
-export default function OrderIndex() {
-
-    return (<div>sss</div>);
-}
-
