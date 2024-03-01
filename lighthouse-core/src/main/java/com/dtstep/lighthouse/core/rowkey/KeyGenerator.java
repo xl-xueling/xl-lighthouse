1727b59b7eedb834b73e33e0551d1f9d7720e509
xl-xueling <better_xueling@126.com>
2024-03-01 17:06:25 +0800

    light insights project init

--- lighthouse-core/src/main/java/com/dtstep/lighthouse/core/rowkey/KeyGenerator.java
+++ lighthouse-core/src/main/java/com/dtstep/lighthouse/core/rowkey/KeyGenerator.java
@@ -1,13 +1,11 @@
 package com.dtstep.lighthouse.core.rowkey;
 
-import com.dtstep.lighthouse.common.entity.group.GroupExtEntity;
-import com.dtstep.lighthouse.common.entity.stat.StatExtEntity;
 import com.dtstep.lighthouse.common.modal.Group;
 import com.dtstep.lighthouse.common.modal.Stat;
 
 public interface KeyGenerator {
 
-    String resultKey(Stat stat, int functionIndex, String dimens, long batchTime);
+    String resultKey(Stat stat, int functionIndex, String dimensValue, long batchTime);
 
     String dimensKey(Group group, String dimens, String dimensValue);
 }
