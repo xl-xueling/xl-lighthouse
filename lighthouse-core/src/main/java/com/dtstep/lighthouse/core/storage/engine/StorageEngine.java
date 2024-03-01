68528116eb148c131ba4d4319644254ce0f97686
xl-xueling <better_xueling@126.com>
2024-03-01 13:26:26 +0800

    light insights project init

--- lighthouse-core/src/main/java/com/dtstep/lighthouse/core/storage/engine/StorageEngine.java
+++ lighthouse-core/src/main/java/com/dtstep/lighthouse/core/storage/engine/StorageEngine.java
@@ -4,13 +4,22 @@ import java.util.List;
 
 public interface StorageEngine {
 
-    void put(String metaName, Put put) throws Exception;
+    int createNamespace(String namespace) throws Exception;
 
-    void puts(String metaName, List<Put> putList) throws Exception;
+    int createTable(String tableName) throws Exception;
 
-    Result get(String metaName, Get get) throws Exception;
+    int dropTable(String tableName) throws Exception;
 
-    List<Result> gets(String metaName, List<Get> gets) throws Exception;
+    int put(String tableName, Put put) throws Exception;
 
-    List<Result> scan(String metaName,String startRow,String endRow,int limit) throws Exception;
+    int puts(String tableName, List<Put> putList) throws Exception;
+
+    Result get(String tableName, Get get) throws Exception;
+
+    List<Result> gets(String tableName, List<Get> gets) throws Exception;
+
+    List<Result> scan(String tableName,String startRow,String endRow,int limit) throws Exception;
+
+    int delete(String tableName,String key) throws Exception;
+
 }
