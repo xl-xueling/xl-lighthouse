f775b19720a30ced27dc0ba9655da8249358e41c
xl-xueling <better_xueling@126.com>
2024-03-08 21:29:49 +0800

    light insights project init

---
+++ lighthouse-core/src/test/java/com/dtstep/lighthouse/core/test/stat/SendTest.java
@@ -0,0 +1,22 @@
+package com.dtstep.lighthouse.core.test.stat;
+
+import com.dtstep.lighthouse.client.LightHouse;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SendTest {
+
+    @Test
+    public void testCount() throws Exception{
+        LightHouse.init("10.206.6.13:4061");
+        for(int i=0;i<100;i++){
+            Map<String,Object> map = new HashMap<>();
+            LightHouse.stat("Cim:test_stat","eL23ySRbVto4KkT4pbcqfQbdV3Ylkl1MmVsnLgRb",map,System.currentTimeMillis());
+        }
+        System.out.println("send success!");
+        Thread.sleep(100000);
+
+    }
+}
