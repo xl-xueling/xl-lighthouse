02d129e5f09bf6848d68aa0c4df2bf787ae51818
xl-xueling <better_xueling@126.com>
2024-03-11 12:51:22 +0800

    light insights project init

--- lighthouse-core/src/test/java/com/dtstep/lighthouse/core/test/builtin/BuiltinLoaderTest.java
+++ lighthouse-core/src/test/java/com/dtstep/lighthouse/core/test/builtin/BuiltinLoaderTest.java
@@ -2,16 +2,20 @@ package com.dtstep.lighthouse.core.test.builtin;
 
 import com.dtstep.lighthouse.common.entity.group.GroupExtEntity;
 import com.dtstep.lighthouse.common.entity.stat.StatExtEntity;
+import com.dtstep.lighthouse.common.random.RandomID;
 import com.dtstep.lighthouse.common.util.JsonUtil;
 import com.dtstep.lighthouse.core.builtin.BuiltinLoader;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.UUID;
 
 public class BuiltinLoaderTest {
 
     @Test
     public void testLoadGroup() throws Exception {
+        String s = RandomID.id(32);
+        System.out.println(s);
         List<GroupExtEntity> groupExtEntityList = BuiltinLoader.getAllGroups();
         for(int i=0;i<groupExtEntityList.size();i++){
             System.out.println("group:" + JsonUtil.toJSONString(groupExtEntityList.get(i)));
