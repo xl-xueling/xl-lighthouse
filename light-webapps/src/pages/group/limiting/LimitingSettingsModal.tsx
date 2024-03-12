060b4eee6227694118f019ac9086aa7ba84da240
xl-xueling <better_xueling@126.com>
2024-03-12 22:20:04 +0800

    light insights project init

--- light-webapps/src/pages/group/limiting/LimitingSettingsModal.tsx
+++ light-webapps/src/pages/group/limiting/LimitingSettingsModal.tsx
@@ -2,35 +2,33 @@ import React, {useEffect, useRef, useState} from 'react';
 import {
     Button, Card, Collapse,
     Form,
-    Grid,
     Input, InputNumber,
-    Message,
-    Modal,
-    Notification, PaginationProps,
-    Radio, Select, Space, Table, TableColumnProps,
-    Tabs,
-    TreeSelect,
-    Typography
+    Modal, Select, Space,
 } from "@arco-design/web-react";
-import {Record, User} from "@/types/insights-web";
-import {requestList} from "@/api/record";
-import {RecordTypeEnum, ResourceTypeEnum, ResultData} from "@/types/insights-common";
-import {GlobalErrorCodes} from "@/utils/constants";
+import {RecordTypeEnum, ResourceTypeEnum} from "@/types/insights-common";
 import useLocale from "@/utils/useLocale";
 import locale from "./locale";
 import "./style/index.module.less"
-import {formatTimeStampBackUp, getRandomString} from "@/utils/util";
-import {LimitedRecord, translateRecord} from "@/pages/record/record";
 import {LimitedRecordModal} from "@/pages/record/limited_records";
-import DepartmentsTransfer from "@/pages/components/transfer/department_transfer";
-import UsersTransfer from "@/pages/components/transfer/user_transfer";
-
 
 export function LimitingSettingsModal({groupInfo,onClose}){
 
     const t = useLocale(locale);
     const CollapseItem = Collapse.Item;
     const FormItem = Form.Item;
+    const formRef = useRef(null);
+
+    const changeSelect = (v) => {
+        if(v == 1){
+            formRef.current.setFieldsValue({
+                currentValue: groupInfo?.extendConfig?.limitingConfig.GROUP_MESSAGE_SIZE_LIMITING
+            });
+        }else{
+            formRef.current.setFieldsValue({
+                currentValue: groupInfo?.extendConfig?.limitingConfig.STAT_RESULT_SIZE_LIMITING
+            });
+        }
+    }
 
     return (
         <Modal
@@ -44,14 +42,16 @@ export function LimitingSettingsModal({groupInfo,onClose}){
             <Space direction={"vertical"} style={{width:'100%',border:'1px solid var(--color-border)'}}>
                 <LimitedRecordModal resourceId={groupInfo?.id} recordTypes={[RecordTypeEnum.GROUP_MESSAGE_LIMITED]} resourceType={ResourceTypeEnum.Group} />
                 <Collapse style={{marginTop:'10px',borderLeft:"none",borderRight:"none"}}>
-                    <CollapseItem style={{borderLeft:"none",borderRight:"none",textAlign:'center'}} header={<span>限流阈值修改</span>}
+                    <CollapseItem style={{borderLeft:"none",borderRight:"none",textAlign:'center'}} header={<span>{t['limitingConfig.collapse.title']}</span>}
                                   name='1'>
                         <Form
+                            ref={formRef}
                             autoComplete='off'
+                            colon={'：'}
                             labelCol={{ span: 8 }}
                             wrapperCol={{ span: 16 }}
                             style={{ width: '75%',textAlign:'center',marginTop:'25px'}}
-                            initialValues={{ name: 'admin',currentValue:'2300' }}
+                            initialValues={{ strategy: '1',currentValue:groupInfo?.extendConfig?.limitingConfig.GROUP_MESSAGE_SIZE_LIMITING }}
                             onValuesChange={(v, vs) => {
                                 console.log(v, vs);
                             }}
@@ -66,6 +66,9 @@ export function LimitingSettingsModal({groupInfo,onClose}){
                             <FormItem label={t['limitingConfig.form.label.strategy']} field='strategy' rules={[{ required: true }]}>
                                 <Select
                                     placeholder='Please select'
+                                    onChange={
+                                        changeSelect
+                                    }
                                 >
                                     <Select.Option key={1} value={'1'}>
                                         {t['basic.limitingStrategy.messageSizeLimiting']}
@@ -95,12 +98,12 @@ export function LimitingSettingsModal({groupInfo,onClose}){
                                     htmlType='submit'
                                     style={{ marginRight: 24 }}
                                 >
-                                    Submit
+                                    {t['basic.form.button.submit']}
                                 </Button>
                                 <Button
                                     style={{ marginRight: 24 }}
                                 >
-                                    Reset
+                                    {t['basic.form.button.reset']}
                                 </Button>
                             </FormItem>
                         </Form>
--- light-webapps/src/pages/group/limiting/locale/index.ts
+++ light-webapps/src/pages/group/limiting/locale/index.ts
@@ -4,12 +4,14 @@ const i18n = {
         'limitingConfig.form.label.strategy':'Strategy',
         'limitingConfig.form.label.currentValue':'Current Value',
         'limitingConfig.form.label.updateValue':'Update Value',
+        'limitingConfig.collapse.title':'Update Limiting Threshold',
     },
     'zh-CN': {
         'limitingConfig.modal.title':'限流管理',
         'limitingConfig.form.label.strategy':'限流策略',
         'limitingConfig.form.label.currentValue':'当前值',
         'limitingConfig.form.label.updateValue':'更新值',
+        'limitingConfig.collapse.title':'限流阈值修改',
     },
 };
 
