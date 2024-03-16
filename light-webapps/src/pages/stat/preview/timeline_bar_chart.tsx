efff32d191d1d61c78042d28362e840af2dab27d
xl-xueling <better_xueling@126.com>
2024-03-16 18:26:36 +0800

    light insights project init

--- light-webapps/src/pages/stat/preview/timeline_bar_chart.tsx
+++ light-webapps/src/pages/stat/preview/timeline_bar_chart.tsx
@@ -1,50 +1,63 @@
-import React, {useEffect, useRef, useState} from 'react';
-import {useParams} from "react-router-dom";
-import {Stat, StatData, EChartChartValue, StatValue} from "@/types/insights-web";
-import {Notification, Space} from "@arco-design/web-react";
-import useLocale from "@/utils/useLocale";
-import locale from "./locale";
+import React, {useEffect, useState} from 'react';
+import {Space} from "@arco-design/web-react";
 import ReactECharts from 'echarts-for-react';
-import {
-    convertDateToTimestamp,
-    getDailyEndTimestamp,
-    getDailyStartTimestamp,
-    DateFormat, getDayBefore, getDayStartTimestamp, getDayEndTimestamp
-} from "@/utils/date";
-import {formatString, getRandomString, stringifyObj} from "@/utils/util";
-import {getTimeLineBarOption} from "@/pages/stat/preview/common";
+import {getChartLoadingOption, getTimeLineBarOption} from "@/pages/stat/preview/common";
 
-export default function TimeLineBarPanel({data=null,size="default", loading = false,group=null}) {
+export default function TimeLineBarPanel({data = null,size="default", loading = false,group=null}) {
 
     const [timeIndex,setTimeIndex] = useState<number>(1);
-    const [option,setOption] = useState({});
-
-    const loadingOption = {
-        animation: false,
-        icon: 'none',
-        text: 'Loading...',
-        color: '#c23531',
-        showSpinner: true,
-        spinnerRadius: 7,
-        textColor: '#000',
-        fontWeight: 'normal',
-        lineWidth: 2,
-        fontSize: 13,
-        maskColor: 'rgba(255, 255, 255, 1)',
-    };
+    const [seriesArray,setSeriesArray] = useState([]);
+    const [batchList,setBatchList] = useState([]);
+    const [dimensList,setDimensList] = useState([]);
 
     const handleTimelineClick = (e) => {
         console.log("e is:" + e);
     };
 
-
-    useEffect(() => {
-        console.log("timeIndex is:" + timeIndex);
-        if(data){
-            const option = getTimeLineBarOption(data,null,timeIndex);
-            setOption(option);
+    const getSeries = (chartData) => {
+        const seriesArray = new Array<any>();
+        for(let i=0;i<chartData.length;i++){
+            const dimensList = chartData[i].values;
+            const seriesData = [];
+            for(let k=0;k < dimensList.length;k++){
+                seriesData[k] = dimensList[k].score;
+            }
+            const seriesObj = {
+                series: [
+                    {
+                        name: 'Series',
+                        type: 'bar',
+                        data: seriesData,
+                    }
+                ]
+            };
+            seriesArray.push(seriesObj);
         }
-    },[timeIndex])
+        return seriesArray;
+    }
+
+    const defaultOption = {
+        baseOption: {
+            timeline: {
+                axisType: 'category',
+                autoPlay: false,
+                currentIndex:timeIndex,
+                playInterval: 1000,
+                data: batchList,
+                label: {
+                    formatter: '{value}'
+                }
+            },
+            xAxis: {
+                type: 'category',
+                data: dimensList,
+            },
+            yAxis: {
+                type: 'value'
+            },
+        },
+        options: seriesArray,
+    };
 
     const onclick={
         click:(params)=>{
@@ -52,13 +65,22 @@ export default function TimeLineBarPanel({data=null,size="default", loading = fa
         }
     }
 
+    useEffect(() => {
+        if(data){
+            const seriesArray = getSeries(data);
+            setSeriesArray(seriesArray);
+            const batchList = data.map(z => z.displayBatchTime);
+            setBatchList(batchList);
+        }
+    },[data])
+
     const getReactChart = () => {
         if(size == 'default'){
-            return <ReactECharts option={option} onEvents={onclick} />
+            return <ReactECharts option={defaultOption} style={{ height: '300px' ,width:'100%',marginLeft:'0px'}} showLoading={loading} onEvents={onclick} loadingOption={getChartLoadingOption}/>
         }else if(size == 'small'){
-            return <ReactECharts option={option} onEvents={onclick} />
+            return <ReactECharts option={defaultOption} style={{ height: '230px' ,width:'100%',marginLeft:'0px'}} showLoading={loading} onEvents={onclick} loadingOption={getChartLoadingOption}/>
         }else if(size == 'mini'){
-            return <ReactECharts option={option} onEvents={onclick} />
+            return <ReactECharts option={defaultOption} style={{ height: '150px' ,width:'100%',marginLeft:'0px'}} showLoading={loading} onEvents={onclick} loadingOption={getChartLoadingOption}/>
         }
     }
 
