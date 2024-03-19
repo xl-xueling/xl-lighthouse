a96c1db4d899319b402d698087ef3328ea040ca6
xl-xueling <better_xueling@126.com>
2024-03-19 11:09:30 +0800

    light insights project init

--- lighthouse-core/src/main/java/com/dtstep/lighthouse/core/wrapper/StatDBWrapper.java
+++ lighthouse-core/src/main/java/com/dtstep/lighthouse/core/wrapper/StatDBWrapper.java
@@ -45,7 +45,7 @@ public class StatDBWrapper {
 
     private static final Logger logger = LoggerFactory.getLogger(StatDBWrapper.class);
 
-    private static final Integer _CacheExpireMinutes = 50;
+    private static final Integer _CacheExpireMinutes = 5;
 
     private static final LoadingCache<Integer, Optional<StatExtEntity>> statCache = Caffeine.newBuilder()
             .expireAfterWrite(_CacheExpireMinutes, TimeUnit.MINUTES)
