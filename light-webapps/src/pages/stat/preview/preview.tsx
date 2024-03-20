c8ded7f3f2a66d650b81b72bfa0f6099b4120a1e
xl-xueling <better_xueling@126.com>
2024-03-20 14:13:56 +0800

    light insights project init

--- light-webapps/src/pages/stat/preview/preview.tsx
+++ light-webapps/src/pages/stat/preview/preview.tsx
@@ -178,7 +178,7 @@ export default function StatPreviewPanel({specifyTitle = null,size = 'default',i
         const pageTitle = getPageTitle();
         setPageTitle(pageTitle);
         fetchStatData().then();
-        if(statInfo.templateEntity.limitFlag){
+        if(statInfo?.templateEntity.limitFlag){
             fetchLimitData().then();
         }
     },[statInfo])
