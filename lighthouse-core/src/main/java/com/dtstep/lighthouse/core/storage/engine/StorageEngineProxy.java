556ff6c72bf4df9d1d5a2bb8106e512e4829888c
xl-xueling <better_xueling@126.com>
2024-03-26 12:32:03 +0800

    light insights project init

--- lighthouse-core/src/main/java/com/dtstep/lighthouse/core/storage/engine/StorageEngineProxy.java
+++ lighthouse-core/src/main/java/com/dtstep/lighthouse/core/storage/engine/StorageEngineProxy.java
@@ -1,10 +1,11 @@
 package com.dtstep.lighthouse.core.storage.engine;
 
-import com.dtstep.lighthouse.core.storage.engine.hbase.HBaseStorageEngine;
+
+import com.dtstep.lighthouse.core.storage.engine.mysql.MySQLStorageEngine;
 
 public class StorageEngineProxy {
 
-    private static final StorageEngine storageEngine = new HBaseStorageEngine();
+    private static final StorageEngine storageEngine = new MySQLStorageEngine();
 
     public static StorageEngine getInstance(){
         return storageEngine;
