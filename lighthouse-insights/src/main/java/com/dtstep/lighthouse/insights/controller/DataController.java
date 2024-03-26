a56840f746c2960f5b4580894a4daf9ca9c76a0c
xl-xueling <better_xueling@126.com>
2024-03-26 14:42:52 +0800

    light insights project init

--- lighthouse-insights/src/main/java/com/dtstep/lighthouse/insights/controller/DataController.java
+++ lighthouse-insights/src/main/java/com/dtstep/lighthouse/insights/controller/DataController.java
@@ -17,6 +17,7 @@ package com.dtstep.lighthouse.insights.controller;
  * limitations under the License.
  */
 import com.dtstep.lighthouse.common.entity.stat.StatExtEntity;
+import com.dtstep.lighthouse.common.enums.PrivateTypeEnum;
 import com.dtstep.lighthouse.common.enums.RoleTypeEnum;
 import com.dtstep.lighthouse.common.modal.LimitDataObject;
 import com.dtstep.lighthouse.common.modal.Role;
@@ -64,7 +65,9 @@ public class DataController {
     @PostMapping("/data/stat")
     public ResultData<List<StatDataObject>> dataQuery(@Validated @RequestBody DataStatQueryParam queryParam) throws Exception{
         int statId = queryParam.getStatId();
-        if(!BuiltinLoader.isBuiltinStat(statId)){
+        StatExtEntity stat = statService.queryById(statId);
+        Validate.notNull(stat);
+        if(!BuiltinLoader.isBuiltinStat(statId) && stat.getPrivateType() == PrivateTypeEnum.Private){
             int userId = baseService.getCurrentUserId();
             Role manageRole = roleService.queryRole(RoleTypeEnum.STAT_MANAGE_PERMISSION,statId);
             boolean hasManagePermission = permissionService.checkUserPermission(userId,manageRole.getId());
@@ -76,8 +79,6 @@ public class DataController {
                 }
             }
         }
-        StatExtEntity stat = statService.queryById(statId);
-        Validate.notNull(stat);
         List<String> dimensList = dataService.dimensArrangement(stat,queryParam.getDimensParams());
         if(stat.getTemplateEntity().getDimensArray().length > 0 && CollectionUtils.isEmpty(dimensList)){
             return ResultData.result(ResultCode.dataQueryMissingDimensParams);
@@ -91,18 +92,7 @@ public class DataController {
         int statId = queryParam.getStatId();
         StatExtEntity stat = statService.queryById(statId);
         Validate.notNull(stat);
-        List<Long> batchTimeList = queryParam.getBatchTimeList();
-        if(CollectionUtils.isEmpty(batchTimeList)){
-            batchTimeList = BatchAdapter.queryBatchTimeList(stat.getTimeparam(),0,System.currentTimeMillis(),10);
-        }
-        List<LimitDataObject> objectList = dataService.limitQuery(stat,batchTimeList);
-        return ResultData.success(objectList);
-    }
-
-    @PostMapping("/test-data/stat")
-    public ResultData<List<StatDataObject>> testDataQuery(@Validated @RequestBody DataStatQueryParam queryParam) throws Exception{
-        int statId = queryParam.getStatId();
-        if(!BuiltinLoader.isBuiltinStat(statId)){
+        if(!BuiltinLoader.isBuiltinStat(statId) && stat.getPrivateType() == PrivateTypeEnum.Private){
             int userId = baseService.getCurrentUserId();
             Role manageRole = roleService.queryRole(RoleTypeEnum.STAT_MANAGE_PERMISSION,statId);
             boolean hasManagePermission = permissionService.checkUserPermission(userId,manageRole.getId());
@@ -114,10 +104,58 @@ public class DataController {
                 }
             }
         }
+        List<Long> batchTimeList = queryParam.getBatchTimeList();
+        if(CollectionUtils.isEmpty(batchTimeList)){
+            batchTimeList = BatchAdapter.queryBatchTimeList(stat.getTimeparam(),0,System.currentTimeMillis(),10);
+        }
+        List<LimitDataObject> objectList = dataService.limitQuery(stat,batchTimeList);
+        return ResultData.success(objectList);
+    }
+
+    @PostMapping("/test-data/stat")
+    public ResultData<List<StatDataObject>> testDataQuery(@Validated @RequestBody DataStatQueryParam queryParam) throws Exception{
+        int statId = queryParam.getStatId();
         StatExtEntity statExtEntity = statService.queryById(statId);
+        Validate.notNull(statExtEntity);
+        if(!BuiltinLoader.isBuiltinStat(statId) && statExtEntity.getPrivateType() == PrivateTypeEnum.Private){
+            int userId = baseService.getCurrentUserId();
+            Role manageRole = roleService.queryRole(RoleTypeEnum.STAT_MANAGE_PERMISSION,statId);
+            boolean hasManagePermission = permissionService.checkUserPermission(userId,manageRole.getId());
+            if(!hasManagePermission){
+                Role accessRole = roleService.queryRole(RoleTypeEnum.STAT_ACCESS_PERMISSION,statId);
+                boolean hasAccessPermission = permissionService.checkUserPermission(userId,accessRole.getId());
+                if(!hasAccessPermission){
+                    return ResultData.result(ResultCode.accessDenied);
+                }
+            }
+        }
         List<String> dimensList = dataService.dimensArrangement(statExtEntity,queryParam.getDimensParams());
         List<StatDataObject> objectList = dataService.testDataQuery(statExtEntity,queryParam.getStartTime(),queryParam.getEndTime(),dimensList);
-        System.out.println("objectList is:" + JsonUtil.toJSONString(objectList));
+        return ResultData.success(objectList);
+    }
+
+    @PostMapping("/test-data/limit")
+    public ResultData<List<LimitDataObject>> testLimitQuery(@Validated @RequestBody LimitStatQueryParam queryParam) throws Exception {
+        int statId = queryParam.getStatId();
+        StatExtEntity stat = statService.queryById(statId);
+        Validate.notNull(stat);
+        if(!BuiltinLoader.isBuiltinStat(statId) && stat.getPrivateType() == PrivateTypeEnum.Private){
+            int userId = baseService.getCurrentUserId();
+            Role manageRole = roleService.queryRole(RoleTypeEnum.STAT_MANAGE_PERMISSION,statId);
+            boolean hasManagePermission = permissionService.checkUserPermission(userId,manageRole.getId());
+            if(!hasManagePermission){
+                Role accessRole = roleService.queryRole(RoleTypeEnum.STAT_ACCESS_PERMISSION,statId);
+                boolean hasAccessPermission = permissionService.checkUserPermission(userId,accessRole.getId());
+                if(!hasAccessPermission){
+                    return ResultData.result(ResultCode.accessDenied);
+                }
+            }
+        }
+        List<Long> batchTimeList = queryParam.getBatchTimeList();
+        if(CollectionUtils.isEmpty(batchTimeList)){
+            batchTimeList = BatchAdapter.queryBatchTimeList(stat.getTimeparam(),0,System.currentTimeMillis(),10);
+        }
+        List<LimitDataObject> objectList = dataService.testLimitQuery(stat,batchTimeList);
         return ResultData.success(objectList);
     }
 }
