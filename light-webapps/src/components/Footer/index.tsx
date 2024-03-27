b863227311c449aefdb2bb13e8041bdf8891e96c
xl-xueling <better_xueling@126.com>
2024-03-27 12:02:12 +0800

    light insights project init

--- light-webapps/src/components/Footer/index.tsx
+++ light-webapps/src/components/Footer/index.tsx
@@ -3,12 +3,14 @@ import { Layout } from '@arco-design/web-react';
 import { FooterProps } from '@arco-design/web-react/es/Layout/interface';
 import cs from 'classnames';
 import styles from './style/index.module.less';
+import {getDateFormat, YearFormat} from "@/utils/date";
 
 function Footer(props: FooterProps = {}) {
   const { className, ...restProps } = props;
   return (
     <Layout.Footer className={cs(styles.footer, className)} {...restProps}>
-        {/*Copyright &copy; 2023-2024 XL-LightHouse.*/}
+      Copyright &copy; {getDateFormat(YearFormat)} XueLing All Rights Reserved.
+      <span style={{marginLeft:'10px'}}>Version:2.1.0</span>
     </Layout.Footer>
   );
 }
