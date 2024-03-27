6b9a506d60c847591da34b4500e00bbe263863df
xl-xueling <better_xueling@126.com>
2024-03-27 09:18:53 +0800

    light insights project init

--- lighthouse-insights/src/test/java/com/dtstep/lighthouse/insights/test/utils/TestUtil.java
+++ lighthouse-insights/src/test/java/com/dtstep/lighthouse/insights/test/utils/TestUtil.java
@@ -4,6 +4,8 @@ import com.dtstep.lighthouse.common.util.JsonUtil;
 import com.dtstep.lighthouse.common.modal.PermissionEnum;
 import com.dtstep.lighthouse.common.modal.Order;
 import com.dtstep.lighthouse.common.modal.Project;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.junit.Test;
 
 import java.util.HashSet;
@@ -46,6 +48,40 @@ public class TestUtil {
         Object obj = null;
         Project project = (Project) obj;
         System.out.println("project:" + project);
+    }
+
+    @Test
+    public void test2() throws Exception {
+        ArrayNode arrayNode = JsonUtil.createArrayNode();
+        ObjectNode objectNode1 = JsonUtil.createObjectNode();
+        objectNode1.put("label","北京");
+        objectNode1.put("value","1");
+        arrayNode.add(objectNode1);
+        ArrayNode arrayNode1 = JsonUtil.createArrayNode();
+        ObjectNode objectNode11 = JsonUtil.createObjectNode();
+        objectNode11.put("label","朝阳");
+        objectNode11.put("value","11");
+        arrayNode1.add(objectNode11);
+        ObjectNode objectNode12 = JsonUtil.createObjectNode();
+        objectNode12.put("label","海淀");
+        objectNode12.put("value","12");
+        arrayNode1.add(objectNode12);
+        objectNode1.set("children",arrayNode1);
+        ObjectNode objectNode2 = JsonUtil.createObjectNode();
+        objectNode2.put("label","上海");
+        objectNode2.put("value","2");
+        arrayNode.add(objectNode2);
+        ArrayNode arrayNode2 = JsonUtil.createArrayNode();
+        ObjectNode objectNode21 = JsonUtil.createObjectNode();
+        objectNode21.put("label","浦东");
+        objectNode21.put("value","21");
+        arrayNode2.add(objectNode21);
+        ObjectNode objectNode22 = JsonUtil.createObjectNode();
+        objectNode22.put("label","静安");
+        objectNode22.put("value","22");
+        arrayNode2.add(objectNode22);
+        objectNode2.set("children",arrayNode2);
+        System.out.println(arrayNode.toString());
     }
 
 }
