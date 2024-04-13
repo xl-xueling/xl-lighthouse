34443345ae1d94b2321bc712cffb778668bc498f
xl-xueling <better_xueling@126.com>
2024-04-13 16:48:51 +0800

    light insights project init

--- lighthouse-core/src/test/java/com/dtstep/lighthouse/core/test/stat/HelloWorld.java
+++ lighthouse-core/src/test/java/com/dtstep/lighthouse/core/test/stat/HelloWorld.java
@@ -13,7 +13,9 @@ import java.util.concurrent.ThreadLocalRandom;
 
 /**
  *
- * 为了测试方便，您可以直接下载工程代码，在Web服务中依次创建(统计工程 -> 统计组 -> 统计项)后，然后使用com.dtstep.lighthouse.core.test.stat.HelloWorld中的单元测试方法，记得修改RPC服务IP地址和统计组的Token及秘钥，运行即可！
+ * 为了测试方便，在Web服务中依次创建(统计工程 -> 统计组 -> 统计项)后，您可以直接下载工程代码，然后使用com.dtstep.lighthouse.core.test.stat.HelloWorld中的单元测试方法，记得修改RPC服务IP地址和统计组的Token及秘钥，运行即可！
+ *
+ * Web端页面更新约有1分钟左右延时，请耐心等待！
  *
  * 使用过程中如有问题，及时反馈，本人一定第一时间响应~
  *
