d756a3314d4d290b7169cf283809e6b0010bc11a
xl-xueling <better_xueling@126.com>
2024-04-22 09:53:59 +0800

    dev:Modify maven configuration.

--- light-webapps/src/components/Footer/index.tsx
+++ light-webapps/src/components/Footer/index.tsx
@@ -10,7 +10,7 @@ function Footer(props: FooterProps = {}) {
   return (
     <Layout.Footer className={cs(styles.footer, className)} {...restProps}>
       Copyright &copy; {getDateFormat(YearFormat)} XueLing All Rights Reserved.
-      <span style={{marginLeft:'10px'}}>Version:2.1.5</span>
+      <span style={{marginLeft:'10px'}}>Version:2.1.6</span>
     </Layout.Footer>
   );
 }
