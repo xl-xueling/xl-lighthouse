ac837d3511804c300da6c51a35fbfafec08ef347
xl-xueling <better_xueling@126.com>
2024-05-06 09:54:20 +0800

    fixed:Modify hadoop-auth 401 unauthorized issue.

--- lighthouse-upgrade/upgrade.sh
+++ lighthouse-upgrade/upgrade.sh
@@ -26,10 +26,10 @@ function temporarySync() {
     local IPArray=($(getServiceIPS 'hadoop'))
         for ip in "${IPArray[@]}"
                 do
-                  remoteExecute ${CUR_DIR}/common/delete.exp ${CUR_USER} ${ip} ${NODES_MAP[$ip]} ${hadoop_home}/share/hadoop/common/lib/hadoop-auth-*.jar
-                  remoteExecute ${CUR_DIR}/common/delete.exp ${CUR_USER} ${ip} ${NODES_MAP[$ip]} ${hadoop_home}/share/hadoop/hdfs/lib/hadoop-auth-*.jar
-                  remoteExecute ${CUR_DIR}/common/sync.exp ${CUR_USER}  ${UPGRADE_HOME}/temporary/hadoop-auth-*.jar ${ip} ${NODES_MAP[$ip]} ${hadoop_home}/share/hadoop/common/lib/
-                  remoteExecute ${CUR_DIR}/common/sync.exp ${CUR_USER}  ${UPGRADE_HOME}/temporary/hadoop-auth-*.jar ${ip} ${NODES_MAP[$ip]} ${hadoop_home}/share/hadoop/hdfs/lib/
+                  remoteExecute ${CUR_DIR}/common/delete.exp ${CUR_USER} ${ip} ${NODES_MAP[$ip]} ${hadoop_home}/share/hadoop/common/lib/hadoop-auth-3.3.5.jar
+                  remoteExecute ${CUR_DIR}/common/delete.exp ${CUR_USER} ${ip} ${NODES_MAP[$ip]} ${hadoop_home}/share/hadoop/hdfs/lib/hadoop-auth-3.3.5.jar
+                  remoteExecute ${CUR_DIR}/common/sync.exp ${CUR_USER}  ${LDP_HOME}/plugins/hadoop-auth/*.jar ${ip} ${NODES_MAP[$ip]} ${hadoop_home}/share/hadoop/common/lib/
+                  remoteExecute ${CUR_DIR}/common/sync.exp ${CUR_USER}  ${LDP_HOME}/plugins/hadoop-auth/*.jar ${ip} ${NODES_MAP[$ip]} ${hadoop_home}/share/hadoop/hdfs/lib/
                 done
 }
 
