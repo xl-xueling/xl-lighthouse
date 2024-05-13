827eb4925b7a7bfdd9535fccf3fc68fea9a87d43
xl-xueling <better_xueling@126.com>
2024-05-13 15:35:35 +0800

    feat:create standalone mode.

--- bin/install/base_install.sh
+++ bin/install/base_install.sh
@@ -18,7 +18,7 @@ baseInstallWithYum(){
         sudo yum install -y epel-release
         sudo yum install -y expect jq rsync
 	      sudo yum install -y libtool autoconf gcc gcc-c++ make autoconf automake
-        sudo yum install -y cmake gzip  kernel-devel openssl*
+        sudo yum install -y cmake gzip  kernel-devel openssl openssl-devel
         sudo yum install -y tcl glibc-devel numactl
         sudo yum install -y nc
         sudo yum install -y git maven
