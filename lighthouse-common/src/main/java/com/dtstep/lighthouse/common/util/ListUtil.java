fefc9ead06e2d56a1b816b46ec614196429518b7
xl-xueling <better_xueling@126.com>
2024-05-18 11:39:31 +0800

    feat:create standalone mode.

--- lighthouse-common/src/main/java/com/dtstep/lighthouse/common/util/ListUtil.java
+++ lighthouse-common/src/main/java/com/dtstep/lighthouse/common/util/ListUtil.java
@@ -1,5 +1,8 @@
 package com.dtstep.lighthouse.common.util;
 
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.Validate;
+
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -7,8 +10,11 @@ import java.util.stream.IntStream;
 public class ListUtil {
 
     public static <T> List<List<T>> listPartition(List<T> list, int targetGroupSize) {
-        return IntStream.range(0, (list.size() + targetGroupSize - 1) / targetGroupSize)
-                .mapToObj(i -> list.subList(i * targetGroupSize, Math.min((i + 1) * targetGroupSize, list.size())))
+        Validate.isTrue(CollectionUtils.isNotEmpty(list) && targetGroupSize != 0 && list.size() >= targetGroupSize);
+        int totalSize = list.size();
+        int groupSize = (totalSize + targetGroupSize - 1) / targetGroupSize;
+        return IntStream.range(0, targetGroupSize)
+                .mapToObj(i -> list.subList(i * groupSize, Math.min((i + 1) * groupSize, totalSize)))
                 .collect(Collectors.toList());
     }
 }
--- lighthouse-core/src/main/java/com/dtstep/lighthouse/core/tools/HBaseTableRegionMerge.java
+++ lighthouse-core/src/main/java/com/dtstep/lighthouse/core/tools/HBaseTableRegionMerge.java
@@ -3,8 +3,11 @@ package com.dtstep.lighthouse.core.tools;
 import com.dtstep.lighthouse.core.config.LDPConfig;
 import com.dtstep.lighthouse.core.storage.engine.hbase.HBaseStorageEngine;
 import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.RegionInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 public class HBaseTableRegionMerge {
 
@@ -24,6 +27,12 @@ public class HBaseTableRegionMerge {
         for(TableName tableName : tableNames){
             String tableNameStr = tableName.getNameAsString();
             storageEngine.merge(tableNameStr,targetSize);
+            Thread.sleep(10000);
+            List<RegionInfo> regionInfoList = storageEngine.getRegionInfo(tableNameStr);
+            if(regionInfoList.size() != targetSize){
+                logger.error("Merge table failed,tableName:{},current region size:{},target region size:{}",tableNameStr,regionInfoList.size(),targetSize);
+                break;
+            }
         }
         logger.info("Execution completed and the program exits!");
         System.exit(0);
