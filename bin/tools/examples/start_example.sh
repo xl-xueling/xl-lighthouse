68451b21850bc44ed0f9c92c50a31acba54d66e3
xl-xueling <better_xueling@126.com>
2024-05-27 10:37:33 +0800

    feat:create standalone mode.

---
+++ bin/tools/examples/start_example.sh
@@ -0,0 +1,45 @@
+source ~/.bashrc;
+eval "$(awk '/^export /,0' ~/.bashrc)"
+
+LOCKFILE=/tmp/lighthouse_example.lock
+CMD_PATH=$(cd "$(dirname "$0")";pwd)
+CUR_DIR=$(dirname $(dirname "$CMD_PATH"))
+echo "cur_dir:"${CUR_DIR}
+source "${CUR_DIR}/common/lib.sh"
+source "${CUR_DIR}/prepare/prepare.sh"
+source "${CUR_DIR}/install/install.sh"
+source "${CUR_DIR}/compile/compile.sh"
+source "${CUR_DIR}/deploy/deploy.sh"
+source "${CUR_DIR}/check/check.sh"
+source "${CUR_DIR}/clean/clean.sh"
+source "${CUR_DIR}/run/start.sh"
+source "${CUR_DIR}/run/stop.sh"
+source "${CUR_DIR}/run/reload.sh"
+
+
+main(){
+	[ -e ${LOCKFILE} ] && `cat ${LOCKFILE} | xargs --no-run-if-empty kill -9 >/dev/null 2>&1`;
+    	trap "rm -f ${LOCKFILE}; exit" INT TERM EXIT
+	echo $$ > ${LOCKFILE}
+	prepare;
+	if [[ ${USER} != ${DEPLOY_USER} ]];then
+        	log_error "The operation is prohibited, only user[\"${DEPLOY_USER}\"] is allowed to execute!"
+        	exit -1;
+  	fi
+	if jps -l | grep -q 'LDPFlowTestInstance'; then
+		log_error "The statistical example task is already running, please stop it first!"
+		exit -1;
+	fi	
+	java -Xmx300m -Xms300m -cp ${LDP_HOME}/lib/*:${LDP_HOME}/lib/lighthouse-test-*.jar com.dtstep.lighthouse.test.example.StartExample
+	if [ $? != '0' ];then
+		log_error "Create statistic examples failed,process exit!"
+		exit -1;
+	fi
+	log_info "Waiting for the statistics task to start..."
+	sleep 10;
+	nohup java -Xmx300m -Xms300m -cp ${LDP_HOME}/lib/*:${LDP_HOME}/lib/lighthouse-test-*.jar com.dtstep.lighthouse.test.LDPFlowTestInstance 300 > example.log 2>&1 &
+	log_info "The statistical example task has been started, and the log is being output to the [example.log] file."
+	rm -f ${LOCKFILE}
+}
+
+main $@;
---
+++ bin/tools/examples/stop_example.sh
@@ -0,0 +1,34 @@
+source ~/.bashrc;
+eval "$(awk '/^export /,0' ~/.bashrc)"
+
+LOCKFILE=/tmp/lighthouse_example.lock
+CMD_PATH=$(cd "$(dirname "$0")";pwd)
+CUR_DIR=$(dirname $(dirname "$CMD_PATH"))
+echo "cur_dir:"${CUR_DIR}
+source "${CUR_DIR}/common/lib.sh"
+source "${CUR_DIR}/prepare/prepare.sh"
+source "${CUR_DIR}/install/install.sh"
+source "${CUR_DIR}/compile/compile.sh"
+source "${CUR_DIR}/deploy/deploy.sh"
+source "${CUR_DIR}/check/check.sh"
+source "${CUR_DIR}/clean/clean.sh"
+source "${CUR_DIR}/run/start.sh"
+source "${CUR_DIR}/run/stop.sh"
+source "${CUR_DIR}/run/reload.sh"
+
+
+main(){
+	prepare;
+        if [[ ${USER} != ${DEPLOY_USER} ]];then
+                log_error "The operation is prohibited, only user[\"${DEPLOY_USER}\"] is allowed to execute!"
+                exit -1;
+        fi
+        jps -l|grep 'com.dtstep.lighthouse.test.LDPFlowTestInstance'|grep -v Jps|awk '{print $1}'|xargs --no-run-if-empty kill -9
+	java -Xmx300m -Xms300m -cp ${LDP_HOME}/lib/*:${LDP_HOME}/lib/lighthouse-test-*.jar com.dtstep.lighthouse.test.example.StopExample
+	jps -l|grep 'com.dtstep.lighthouse.test.example.StartExample'|grep -v Jps|awk '{print $1}'|xargs --no-run-if-empty kill -9
+ 	jps -l|grep 'com.dtstep.lighthouse.test.example.StopExample'|grep -v Jps|awk '{print $1}'|xargs --no-run-if-empty kill -9
+	rm -f ./example.log;
+	log_info "The statistical example task has been stopped!"
+}
+
+main $@;
