9efe7ef8b203e54c8b2d25f12bec92cdae21faa6
xl-xueling <better_xueling@126.com>
2024-09-02 11:57:42 +0800

    init

--- light-webapps/src/pages/license/index.tsx
+++ light-webapps/src/pages/license/index.tsx
@@ -5,7 +5,7 @@ import licenseContent from "@/pages/license/license.md?raw";
 function License() {
 
     return (
-        <div style={{paddingLeft:'20px',paddingTop:'20px'}}>
+        <div style={{paddingLeft:'20px',paddingTop:'20px',color:'var(--color-text-1)'}}>
             <Markdown>
                 {licenseContent}
             </Markdown>;
