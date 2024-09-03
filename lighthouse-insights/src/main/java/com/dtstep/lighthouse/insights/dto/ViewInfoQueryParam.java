5c84b8b100d9f6dac2ca44a11e177a24ac942e33
xl-xueling <better_xueling@126.com>
2024-09-03 11:48:10 +0800

    init

--- lighthouse-insights/src/main/java/com/dtstep/lighthouse/insights/dto/ViewInfoQueryParam.java
+++ lighthouse-insights/src/main/java/com/dtstep/lighthouse/insights/dto/ViewInfoQueryParam.java
@@ -4,9 +4,19 @@ import java.io.Serializable;
 
 public class ViewInfoQueryParam implements Serializable {
 
+    private String from = "preview";
+
     private Integer id;
 
     private Integer version;
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
 
     public Integer getId() {
         return id;
