69215fe7f4befed61e5d6e7cd808cceb87bb2852
xl-xueling <better_xueling@126.com>
2024-09-17 17:02:27 +0800

    init

--- lighthouse-core/src/main/java/com/dtstep/lighthouse/core/wrapper/StatDBWrapper.java
+++ lighthouse-core/src/main/java/com/dtstep/lighthouse/core/wrapper/StatDBWrapper.java
@@ -23,6 +23,7 @@ import com.dtstep.lighthouse.common.entity.stat.TemplateEntity;
 import com.dtstep.lighthouse.common.entity.state.StatState;
 import com.dtstep.lighthouse.common.entity.state.StatUnit;
 import com.dtstep.lighthouse.common.enums.GroupStateEnum;
+import com.dtstep.lighthouse.common.enums.PrivateTypeEnum;
 import com.dtstep.lighthouse.common.enums.StatStateEnum;
 import com.dtstep.lighthouse.common.modal.Column;
 import com.dtstep.lighthouse.common.modal.LimitingParam;
@@ -183,6 +184,7 @@ public class StatDBWrapper {
                     LimitingParam statLimitingParam = JsonUtil.toJavaObject(limitingParam, LimitingParam.class);
                     stat.setLimitingParam(statLimitingParam);
                 }
+                int privateType = rs.getInt("private_type");
                 Integer metaId = rs.getInt("meta_id");
                 long createTime = rs.getTimestamp("create_time").getTime();
                 long updateTime = rs.getTimestamp("update_time").getTime();
@@ -199,6 +201,7 @@ public class StatDBWrapper {
                 stat.setExpired(expired);
                 StatStateEnum statStateEnum = StatStateEnum.getByState(state);
                 stat.setState(statStateEnum);
+                stat.setPrivateType(PrivateTypeEnum.forValue(privateType));
                 stat.setMetaId(metaId);
                 stat.setCreateTime(DateUtil.timestampToLocalDateTime(createTime));
                 stat.setUpdateTime(DateUtil.timestampToLocalDateTime(updateTime));
@@ -216,7 +219,7 @@ public class StatDBWrapper {
         QueryRunner queryRunner = new QueryRunner();
         Stat stat;
         try{
-            stat = queryRunner.query(conn, String.format("select a.*,b.token,b.columns from ldp_stats a left join ldp_groups b on a.group_id = b.id where a.id = '%s'",statId), new StatResultSetHandler());
+            stat = queryRunner.query(conn, String.format("select a.*,b.token,b.columns,c.private_type from ldp_stats a left join ldp_groups b on a.group_id = b.id left join ldp_projects c on a.project_id = c.id where a.id = '%s'",statId), new StatResultSetHandler());
         }finally {
             storageEngine.closeConnection();
         }
