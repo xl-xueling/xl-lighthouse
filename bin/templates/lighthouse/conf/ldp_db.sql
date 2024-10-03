90f72931ce6caff45107686211bae1f33a8882d4
xl-xueling <better_xueling@126.com>
2024-10-03 21:03:51 +0800

    feat:Add api caller implementation-checkpoint2.

--- bin/templates/lighthouse/conf/ldp_db.sql
+++ bin/templates/lighthouse/conf/ldp_db.sql
@@ -437,7 +437,7 @@ CREATE TABLE `ldp_callers` (
                                PRIMARY KEY (`id`),
                                KEY `index_name` (`name`),
                                KEY `index_create_time` (`create_time`)
-) ENGINE=InnoDB AUTO_INCREMENT=11001 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=101001 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- bin/templates/lighthouse/conf/ldp_upgrade.sql
+++ bin/templates/lighthouse/conf/ldp_upgrade.sql
@@ -1,37 +1,37 @@
 USE `cluster_${ldp_lighthouse_cluster_id}_ldp_cmdb`;
 
 CREATE TABLE IF NOT EXISTS `ldp_views` (
-                                           `id` int NOT NULL AUTO_INCREMENT,
-                                           `title` varchar(100) NOT NULL,
-    `user_id` int NOT NULL,
-    `state` tinyint(1) NOT NULL,
-    `private_type` tinyint(1) NOT NULL,
-    `version` int NOT NULL,
-    `config` mediumtext,
-    `desc` varchar(500) NOT NULL,
-    `create_time` timestamp NOT NULL,
-    `update_time` timestamp NOT NULL,
-    PRIMARY KEY (`id`),
-    KEY `index_state` (`state`),
-    KEY `index_title` (`title`),
-    KEY `index_create_time` (`create_time`)
-    ) ENGINE=InnoDB AUTO_INCREMENT=1100023 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+                               `id` int NOT NULL AUTO_INCREMENT,
+                               `title` varchar(100) NOT NULL,
+                                `user_id` int NOT NULL,
+                                `state` tinyint(1) NOT NULL,
+                                `private_type` tinyint(1) NOT NULL,
+                                `version` int NOT NULL,
+                                `config` mediumtext,
+                                `desc` varchar(500) NOT NULL,
+                                `create_time` timestamp NOT NULL,
+                                `update_time` timestamp NOT NULL,
+                                PRIMARY KEY (`id`),
+                                KEY `index_state` (`state`),
+                                KEY `index_title` (`title`),
+                                KEY `index_create_time` (`create_time`)
+) ENGINE=InnoDB AUTO_INCREMENT=1100023 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS `ldp_rollbacks` (
-                                               `id` int NOT NULL AUTO_INCREMENT,
-                                               `user_id` int DEFAULT NULL,
-                                               `resource_id` int NOT NULL,
-                                               `data_type` int NOT NULL,
-                                               `config` mediumtext NOT NULL,
-                                               `version` int DEFAULT NULL,
-                                               `create_time` datetime NOT NULL,
-                                               `desc` varchar(500) DEFAULT NULL,
-    `state` tinyint NOT NULL,
-    `extend` varchar(500) DEFAULT NULL,
-    PRIMARY KEY (`id`),
-    KEY `index_resource` (`resource_id`,`data_type`),
-    KEY `index_create_time` (`create_time`)
-    ) ENGINE=InnoDB AUTO_INCREMENT=1100347 DEFAULT CHARSET=utf8mb4;
+                               `id` int NOT NULL AUTO_INCREMENT,
+                               `user_id` int DEFAULT NULL,
+                               `resource_id` int NOT NULL,
+                               `data_type` int NOT NULL,
+                               `config` mediumtext NOT NULL,
+                               `version` int DEFAULT NULL,
+                               `create_time` datetime NOT NULL,
+                               `desc` varchar(500) DEFAULT NULL,
+                                `state` tinyint NOT NULL,
+                                `extend` varchar(500) DEFAULT NULL,
+                                PRIMARY KEY (`id`),
+                                KEY `index_resource` (`resource_id`,`data_type`),
+                                KEY `index_create_time` (`create_time`)
+) ENGINE=InnoDB AUTO_INCREMENT=1100347 DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `ldp_callers` (
                                `id` int NOT NULL AUTO_INCREMENT,
@@ -43,4 +43,4 @@ CREATE TABLE IF NOT EXISTS `ldp_callers` (
                                PRIMARY KEY (`id`),
                                KEY `index_name` (`name`),
                                KEY `index_create_time` (`create_time`)
-) ENGINE=InnoDB AUTO_INCREMENT=11001 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=101001 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
