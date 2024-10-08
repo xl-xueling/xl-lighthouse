874334e24d825c0a5d76002d93798790c8898c05
xl-xueling <better_xueling@126.com>
2024-10-08 13:31:37 +0800

    feat:Add api caller implementation-checkpoint2.

--- light-webapps/src/pages/caller/update/index.tsx
+++ light-webapps/src/pages/caller/update/index.tsx
@@ -1,11 +1,14 @@
 import React, {useEffect, useRef, useState} from 'react';
-import {Form, Input, Modal,Notification} from "@arco-design/web-react";
+import {Form, Input, Modal, Notification, TreeSelect} from "@arco-design/web-react";
 import locale from "./locale/index";
 import {Caller} from "@/types/caller";
 import {requestCreate, requestUpdate} from "@/api/caller";
 import useLocale from "@/utils/useLocale";
 import {TEXT_BASE_PATTERN_4} from "@/utils/constants";
 import {getTextBlenLength} from "@/utils/util";
+import {translateToFlatStruct} from "@/pages/department/base";
+import {useSelector} from "react-redux";
+import {TreeNode} from "@/types/insights-web";
 
 
 export default function CallerUpdateModal({callerInfo = null,onClose,onSuccess}){
@@ -13,6 +16,7 @@ export default function CallerUpdateModal({callerInfo = null,onClose,onSuccess})
     const t = useLocale(locale);
     const formRef = useRef(null);
     const [loading,setLoading] = useState<boolean>(false);
+    const allDepartInfo = useSelector((state: {allDepartInfo:Array<TreeNode>}) => state.allDepartInfo);
 
     async function handlerSubmit(){
         try{
@@ -23,20 +27,17 @@ export default function CallerUpdateModal({callerInfo = null,onClose,onSuccess})
         }
         setLoading(true);
         const values = formRef.current.getFieldsValue();
-        console.log("values is:" + JSON.stringify(values));
         const caller:Caller = {
             id:callerInfo?.id,
             name:'caller:' + values.name,
+            departmentId:values.departmentId,
             desc:values.desc,
         }
-        console.log('caller is:' + JSON.stringify(caller));
         requestUpdate(caller).then((response) => {
-            console.log("---caller is:" + caller);
             const {code, data ,message} = response;
             if(code == '0'){
                 Notification.info({style: { width: 420 }, title: 'Notification', content: t['callerUpdate.form.submit.success']});
                 const newCallerInfo = {...callerInfo,...caller};
-                console.log("newCallerInfo is:" + JSON.stringify(newCallerInfo));
                 onSuccess(newCallerInfo);
                 onClose();
             }else{
@@ -56,7 +57,7 @@ export default function CallerUpdateModal({callerInfo = null,onClose,onSuccess})
 
     return (
         <Modal
-            title= {'修改调用方'}
+            title= {t['callerUpdate.title']}
             alignCenter={false}
             style={{ width:'960px',maxWidth:'80%',verticalAlign:'top', top: '150px' }}
             visible={true}
@@ -70,8 +71,11 @@ export default function CallerUpdateModal({callerInfo = null,onClose,onSuccess})
                 colon={" : "}
                 ref={formRef}
                 labelCol={{span: 4, offset: 0}}
-                initialValues={{name: callerInfo?.name.replace('caller:','') ,desc:callerInfo?.desc}}
-            >
+                initialValues={{
+                    name: callerInfo?.name.replace('caller:',''),
+                    departmentId:callerInfo?.departmentId,
+                    desc:callerInfo?.desc
+                }}>
                 <Form.Item label={t['callerUpdate.form.label.name']} field='name' rules={[
                     { required: true, message: t['basic.form.verification.empty.warning'] , validateTrigger : ['onSubmit']},
                     { required: true, match: new RegExp(TEXT_BASE_PATTERN_4,"g"),message: t['basic.form.verification.failed.warning'] , validateTrigger : ['onSubmit']},
@@ -88,6 +92,24 @@ export default function CallerUpdateModal({callerInfo = null,onClose,onSuccess})
                         , validateTrigger : ['onSubmit']
                     }]}>
                     <Input addBefore={'caller:'} disabled={true} autoFocus={false} />
+                </Form.Item>
+                <Form.Item label={t['callerUpdate.form.label.department']} field="departmentId" rules={[{ required: true ,message: t['callerUpdate.form.department.errMsg'], validateTrigger : ['onSubmit']}]}>
+                    <TreeSelect
+                        disabled={true}
+                        treeProps={{
+                            height: 200,
+                            renderTitle: (props) => {
+                                return (
+                                    <span style={{ whiteSpace: 'nowrap', }} >
+                                            {props.title}
+                                        </span>
+                                );
+                            },
+                        }}
+                        placeholder={"Please Select"}
+                        allowClear={true}
+                        treeData={translateToFlatStruct(allDepartInfo)}
+                    />
                 </Form.Item>
                 <Form.Item label={t['callerUpdate.form.label.desc']} field='desc' rules={[
                     { required: true, message: t['basic.form.verification.empty.warning'] , validateTrigger : ['onSubmit']},
--- light-webapps/src/pages/caller/update/locale/index.ts
+++ light-webapps/src/pages/caller/update/locale/index.ts
@@ -1,13 +1,19 @@
 const i18n = {
   'en-US': {
+    'callerUpdate.title':'Update API Caller',
     'callerUpdate.form.label.name':'Name',
     'callerUpdate.form.label.desc':'Description',
     'callerUpdate.form.submit.success':'Update caller info success!',
+    'callerUpdate.form.label.department':'Department',
+    'callerUpdate.form.department.errMsg': 'Department cannot be empty!',
   },
   'zh-CN': {
+    'callerUpdate.title':'修改调用方',
     'callerUpdate.form.label.name':'名称',
     'callerUpdate.form.label.desc':'描述',
     'callerUpdate.form.submit.success':'修改调用方信息成功！',
+    'callerUpdate.form.label.department':'部门',
+    'callerUpdate.form.department.errMsg': '请选择部门！',
   },
 };
 
