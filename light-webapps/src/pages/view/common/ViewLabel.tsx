518a66267b04f8a471a7b1a45eeb817bb8bc0f04
xl-xueling <better_xueling@126.com>
2024-10-09 10:38:03 +0800

    feat:Add api caller implementation-checkpoint2.

--- light-webapps/src/pages/view/common/ViewLabel.tsx
+++ light-webapps/src/pages/view/common/ViewLabel.tsx
@@ -1,14 +1,33 @@
 import React, {useEffect, useRef, useState} from 'react';
 import {Department, TreeNode, User} from "@/types/insights-web";
 import {Descriptions, Popover} from "@arco-design/web-react";
-import {Props} from "@/pages/department/common/depart";
-
+import useLocale from "@/utils/useLocale";
+import locale from "@/pages/view/common/locale";
 
 export default function ViewLabel({viewInfo}) {
 
+    const t = useLocale(locale);
+
     const getLabel = () => {
         return (
-            <Popover trigger={"click"} content={viewInfo?.title}>
+            <Popover trigger={"click"} content={
+                <Descriptions
+                    size={"mini"}
+                    colon={":"}
+                    column={1}
+                    data={[
+                        {
+                            label: t['viewLabel.label.id'],
+                            value: viewInfo?.id,
+                        },
+                        {
+                            label: t['viewLabel.label.title'],
+                            value: viewInfo?.title,
+                        },
+                    ]}
+                    labelStyle={{ textAlign: 'right', paddingRight: 15}}
+                />
+            }>
                 <span>{viewInfo?.title}</span>
             </Popover>
         );
---
+++ light-webapps/src/pages/view/common/locale/index.tsx
@@ -0,0 +1,12 @@
+const i18n = {
+    'en-US': {
+        'viewLabel.label.id':'ID',
+        'viewLabel.label.title':'Title',
+    },
+    'zh-CN': {
+        'viewLabel.label.id':'ID',
+        'viewLabel.label.title':'名称',
+    },
+};
+
+export default i18n;
