c768f2ce34dd78ac410b6f7f2db1a997989dd09a
xl-xueling <better_xueling@126.com>
2024-10-10 16:09:58 +0800

    feat:Add api caller implementation-checkpoint2.

--- lighthouse-core/src/main/java/com/dtstep/lighthouse/core/wrapper/PermissionDBWrapper.java
+++ lighthouse-core/src/main/java/com/dtstep/lighthouse/core/wrapper/PermissionDBWrapper.java
@@ -1,14 +1,21 @@
 package com.dtstep.lighthouse.core.wrapper;
 
-import com.dtstep.lighthouse.common.modal.Caller;
+import com.dtstep.lighthouse.common.enums.OwnerTypeEnum;
+import com.dtstep.lighthouse.common.enums.RoleTypeEnum;
+import com.dtstep.lighthouse.common.modal.Role;
+import com.dtstep.lighthouse.common.util.DateUtil;
 import com.dtstep.lighthouse.core.storage.cmdb.CMDBStorageEngine;
 import com.dtstep.lighthouse.core.storage.cmdb.CMDBStorageEngineProxy;
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.ResultSetHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
@@ -20,13 +27,69 @@ public class PermissionDBWrapper {
 
     private static final CMDBStorageEngine<Connection> storageEngine = CMDBStorageEngineProxy.getInstance();
 
-    private static final Cache<Object, Optional<Caller>> PERMISSION_CACHE = Caffeine.newBuilder()
+    private static final Cache<Object, Optional<Boolean>> PERMISSION_CACHE = Caffeine.newBuilder()
             .expireAfterWrite(_CacheExpireMinutes, TimeUnit.MINUTES)
             .maximumSize(100000)
             .softValues()
             .build();
 
+    public static boolean hasPermission(Integer ownerId, OwnerTypeEnum ownerTypeEnum,Integer roleId) throws Exception {
+        Optional<Boolean> optional =  PERMISSION_CACHE.get("HasPermission-" + ownerId + "-" + ownerTypeEnum + "-" + roleId, k -> actualHashPermission(ownerId, ownerTypeEnum,roleId));
+        assert optional != null;
+        return optional.orElse(false);
+    }
 
+    public static Optional<Boolean> actualHashPermission(Integer ownerId, OwnerTypeEnum ownerTypeEnum,Integer roleId) {
+        Boolean bool = null;
+        try{
+            bool = checkPermissionFromDB(ownerId,ownerTypeEnum,roleId);
+        }catch (Exception ex){
+            logger.error("check permission info error!", ex);
+        }
+        return Optional.ofNullable(bool);
+    }
 
+    private static Boolean checkPermissionFromDB(Integer ownerId, OwnerTypeEnum ownerTypeEnum,Integer roleId) throws Exception {
+        Connection conn = storageEngine.getConnection();
+        QueryRunner queryRunner = new QueryRunner();
+        Boolean bool;
+        try{
+            bool = queryRunner.query(conn, String.format("WITH RECURSIVE role_hierarchy AS (\n" +
+                    "    SELECT id, pid\n" +
+                    "    FROM ldp_roles\n" +
+                    "    WHERE id = '%s'\n" +
+                    "    UNION ALL\n" +
+                    "    SELECT r.id, r.pid\n" +
+                    "    FROM ldp_roles r\n" +
+                    "    INNER JOIN role_hierarchy rh ON rh.pid = r.id\n" +
+                    ")\n" +
+                    "\n" +
+                    "SELECT \n" +
+                    "    CASE WHEN EXISTS (\n" +
+                    "        SELECT 1\n" +
+                    "        FROM ldp_permissions p\n" +
+                    "        INNER JOIN role_hierarchy rh ON rh.id = p.role_id\n" +
+                    "        WHERE p.owner_id = '%s'\n" +
+                    "        AND p.owner_type = '%s'\n" +
+                    "    )\n" +
+                    "    THEN 'true'\n" +
+                    "    ELSE 'false'\n" +
+                    "    END AS has_permission",roleId,ownerId,ownerTypeEnum.getOwnerType()), new PermissionSetHandler());
+        }finally {
+            storageEngine.closeConnection();
+        }
+        return bool;
+    }
 
+    private static class PermissionSetHandler implements ResultSetHandler<Boolean> {
+
+        @Override
+        public Boolean handle(ResultSet rs) throws SQLException {
+            boolean bool = false;
+            if(rs.next()){
+                bool = rs.getBoolean(1);
+            }
+            return bool;
+        }
+    }
 }
--- lighthouse-core/src/test/java/com/dtstep/lighthouse/core/test/wrapper/PermissionDBWrapperTest.java
+++ lighthouse-core/src/test/java/com/dtstep/lighthouse/core/test/wrapper/PermissionDBWrapperTest.java
@@ -1,6 +1,22 @@
 package com.dtstep.lighthouse.core.test.wrapper;
 
-public class PermissionDBWrapperTest {
+import com.dtstep.lighthouse.common.enums.OwnerTypeEnum;
+import com.dtstep.lighthouse.common.enums.ResourceTypeEnum;
+import com.dtstep.lighthouse.common.modal.Role;
+import com.dtstep.lighthouse.common.util.JsonUtil;
+import com.dtstep.lighthouse.core.test.CoreBaseTest;
+import com.dtstep.lighthouse.core.wrapper.PermissionDBWrapper;
+import com.dtstep.lighthouse.core.wrapper.RoleDBWrapper;
+import org.junit.Test;
 
+public class PermissionDBWrapperTest extends CoreBaseTest {
+
+    @Test
+    public void testHasPermission() throws Exception {
+        for(int i=0;i<100;i++){
+            boolean is = PermissionDBWrapper.hasPermission(11033, OwnerTypeEnum.CALLER,1224);
+            System.out.println("is:" + is);
+        }
+    }
 
 }
