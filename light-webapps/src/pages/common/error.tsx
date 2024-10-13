1bc4fb7a2230fcbb0991578ef4c0f1be1bb66bbc
xl-xueling <better_xueling@126.com>
2024-10-13 22:49:25 +0800

    feat:Add api caller implementation-checkpoint2.

--- light-webapps/src/pages/common/error.tsx
+++ light-webapps/src/pages/common/error.tsx
@@ -4,11 +4,11 @@ import Exception404 from "@/pages/exception/404";
 import Exception500 from "@/pages/exception/500";
 
 
-export default function ErrorPage({errorCode}) {
+export default function ErrorPage({errorCode,fromExternalEmbedding = false}) {
 
     const getErrorPage = () => {
         if(errorCode == '403'){
-            return <Exception403 />
+            return <Exception403 fromExternalEmbedding={fromExternalEmbedding}/>
         }else if(errorCode == '404'){
             return <Exception404 />
         }else {
--- light-webapps/src/pages/exception/403/index.tsx
+++ light-webapps/src/pages/exception/403/index.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Result, Button } from '@arco-design/web-react';
 import locale from './locale';
 import useLocale from '@/utils/useLocale';
 import styles from './style/index.module.less';
 
-function Exception403() {
+function Exception403({fromExternalEmbedding = false}) {
   const t = useLocale(locale);
 
+  useEffect(() => {
+      console.log("fromExternalEmbedding:" + fromExternalEmbedding)
+  },[fromExternalEmbedding])
+
   return (
-      <div className={styles.wrapper}>
+      <div className={fromExternalEmbedding?styles.externalEmbeddingWrapper : styles.wrapper}>
           <Result
               className={styles.result}
               status="403"
