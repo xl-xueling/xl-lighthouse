d0bcf0a19dacd164823712586ea28fc325ee2bd5
xl-xueling <better_xueling@126.com>
2024-10-13 23:10:17 +0800

    feat:Add api caller implementation-checkpoint2.

--- light-webapps/src/utils/request.ts
+++ light-webapps/src/utils/request.ts
@@ -66,44 +66,5 @@ export const request = async <T>(config): Promise<ResultData<T>> => {
     return result;
 }
 
-export interface CallerInfo {
-    callerName?:string,
-    callerKey?:string,
-}
-
-export interface EnvConfig {
-    REACT_APP_BASE_URL?:string,
-    AXIOS_TIMEOUT?:number,
-}
-
-export const callerRequest = async <T>(callerInfo:CallerInfo,envConfig:EnvConfig,config): Promise<ResultData<T>> => {
-    let baseURL = envConfig?.REACT_APP_BASE_URL;
-    const http = axios.create({
-        baseURL: baseURL + '/api/v1',
-        timeout: envConfig?.AXIOS_TIMEOUT,
-    })
-
-    http.interceptors.request.use((config) => {
-        config.headers['Accept-Language'] = 'en-US';
-        config.headers['Caller-Name'] = callerInfo.callerName;
-        config.headers['Caller-Key'] = callerInfo.callerKey;
-        return config;
-    }, (error) => {
-        console.log(error);
-    })
-
-    let result;
-    try{
-        const response: AxiosResponse = await http.request(config);
-        result = response.data;
-    }catch (error) {
-        console.log("Error is:" + error);
-        result = {
-            code: error.response?error.response.status:'500',
-        };
-    }
-    return result;
-}
-
 
 
