54dc12ae4b021b6336f798d4753832867306527d
xl-xueling <better_xueling@126.com>
2024-10-18 23:13:18 +0800

    feat:Add api caller implementation-checkpoint2.

--- light-webapps/src/pages/common/error.tsx
+++ light-webapps/src/pages/common/error.tsx
@@ -4,13 +4,12 @@ import Exception404 from "@/pages/exception/404";
 import Exception500 from "@/pages/exception/500";
 import Exception100 from "@/pages/exception/100";
 
-
 export default function ErrorPage({errorCode,fromExternalEmbedding = false,errorMessage = null}) {
 
     const getErrorPage = () => {
         if(errorCode == '100'){
             return <Exception100 fromExternalEmbedding={fromExternalEmbedding} errorMessage={errorMessage}/>
-        }else if(errorCode == '403'){
+        }else if(errorCode == '401' || errorCode == '403'){
             return <Exception403 fromExternalEmbedding={fromExternalEmbedding} errorMessage={errorMessage}/>
         }else if(errorCode == '404'){
             return <Exception404 fromExternalEmbedding={fromExternalEmbedding} errorMessage={errorMessage}/>
