0200efa8ed35a45c6438b8ad21a3259ab9e5a2c8
xl-xueling <better_xueling@126.com>
2024-10-19 16:52:55 +0800

    feat:Add api caller implementation-checkpoint2.

--- lighthouse-core/src/test/java/com/dtstep/lighthouse/core/test/api/RpcAPITest.java
+++ lighthouse-core/src/test/java/com/dtstep/lighthouse/core/test/api/RpcAPITest.java
@@ -1,4 +1,34 @@
 package com.dtstep.lighthouse.core.test.api;
 
+import com.dtstep.lighthouse.client.LightHouse;
+import com.dtstep.lighthouse.common.random.RandomID;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class RpcAPITest {
+
+    static {
+        try{
+            //修改rpc服务注册中心地址,集群模式为一主一从，默认为部署集群的前两个节点IP,使用逗号分割，单机模式为当前节点IP
+            //LightHouse.init("10.206.6.11:4061,10.206.6.12:4061");//集群模式初始化
+            LightHouse.init("10.206.6.31:4061");//单机模式初始化
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    @Test
+    public void orderStat() throws Exception {
+        long t = System.currentTimeMillis();
+        HashMap<String,Object> paramMap = new HashMap<>();
+        paramMap.put("imei", RandomID.id(6));
+        paramMap.put("biz", RandomID.id(6));
+        Double d = ThreadLocalRandom.current().nextDouble(1000);
+        paramMap.put("amount",String.format("%.3f", d));//防止上面随机数出现科学计数法
+        LightHouse.stat("N4C:test_order","mfbWuKc17e8hGNwGRlR2JGSfS2GgvqD0wIxjMuqm",paramMap,t);
+        System.out.println("send ok!");
+        Thread.sleep(30000);//client为异步发送，防止进程结束时内存中部分消息没有发送出去
+    }
 }
--- lighthouse-insights/src/test/java/com/dtstep/lighthouse/insights/test/utils/TestAviator.java
+++ lighthouse-insights/src/test/java/com/dtstep/lighthouse/insights/test/utils/TestAviator.java
@@ -23,3 +23,5 @@ public class TestAviator {
         System.out.println("obj:" + obj);
     }
 }
+
+
