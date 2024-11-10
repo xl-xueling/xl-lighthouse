43210710fc47dcf07325cb94dec2ef744bcce108
xl-xueling <better_xueling@126.com>
2024-11-10 21:08:07 +0800

    feat:alarm module development.

--- lighthouse-insights/src/main/java/com/dtstep/lighthouse/insights/vo/NotificationVO.java
+++ lighthouse-insights/src/main/java/com/dtstep/lighthouse/insights/vo/NotificationVO.java
@@ -5,8 +5,18 @@ import com.dtstep.lighthouse.common.util.BeanCopyUtil;
 
 public class NotificationVO extends Notification {
 
+    private Object extend;
+
     public NotificationVO(Notification notification){
         assert notification != null;
         BeanCopyUtil.copy(notification,this);
+    }
+
+    public Object getExtend() {
+        return extend;
+    }
+
+    public void setExtend(Object extend) {
+        this.extend = extend;
     }
 }
