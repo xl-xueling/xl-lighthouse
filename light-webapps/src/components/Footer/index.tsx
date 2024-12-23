ff1393d086dc85195a5c1b3188c5b16d10646fa7
xl-xueling <better_xueling@126.com>
2024-12-23 10:41:46 +0800

    feat:SDK support golang.

--- light-webapps/src/components/Footer/index.tsx
+++ light-webapps/src/components/Footer/index.tsx
@@ -11,7 +11,7 @@ function Footer(props: FooterProps = {}) {
   return (
     <Layout.Footer className={cs(styles.footer, className)} {...restProps}>
         <span style={{cursor:"pointer"}} onClick={() => windowRedirect('/license')}>Copyright &copy; {getDateFormat(YearFormat)} XueLing All Rights Reserved.</span>
-      <span style={{marginLeft:'10px'}}>Version:2.3.1</span>
+      <span style={{marginLeft:'10px'}}>Version:2.3.2</span>
     </Layout.Footer>
   );
 }
