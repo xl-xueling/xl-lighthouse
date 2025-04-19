489eda9723e3100662046a3529c17742d4429217
xl-xueling <better_xueling@126.com>
2025-04-19 17:11:25 +0800

    feat:Support Aliyun Cloud alarm API.

--- light-webapps/src/components/Footer/index.tsx
+++ light-webapps/src/components/Footer/index.tsx
@@ -10,7 +10,7 @@ function Footer(props: FooterProps = {}) {
   const { className, ...restProps } = props;
   return (
     <Layout.Footer className={cs(styles.footer, className)} {...restProps}>
-        <span style={{cursor:"pointer"}} onClick={() => windowRedirect('/license')}>Copyright &copy; {getDateFormat(YearFormat)} XueLing All Rights Reserved.</span>
+        <span style={{cursor:"pointer"}} onClick={() => windowRedirect('/license')}>Copyright &copy; {getDateFormat(YearFormat)} DTStep.com All Rights Reserved.</span>
       <span style={{marginLeft:'10px'}}>Version:2.3.3</span>
     </Layout.Footer>
   );
