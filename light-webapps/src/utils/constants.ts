d5a26d643615e1c4311df9b72cd7a51f6abb47d1
xl-xueling <better_xueling@126.com>
2025-04-20 10:03:44 +0800

    feat:Support Aliyun Cloud alarm API.

--- light-webapps/src/utils/constants.ts
+++ light-webapps/src/utils/constants.ts
@@ -4,6 +4,8 @@ export const TEXT_BASE_PATTERN_1 = /^[a-zA-Z0-9_]+$/;
 export const TEXT_BASE_PATTERN_2 = /^[\u3010\u3011\uFF08\uFF09\u4E00-\u9FA5a-zA-Z0-9_\-()\[\]\/\\#\s]+$/;
 export const TEXT_BASE_PATTERN_3 = /^[\u3010\u3011\uFF08\uFF09\u4E00-\u9FA5a-zA-Z0-9_:\-()\[\]\/\\#\s]+$/;
 export const TEXT_BASE_PATTERN_4 = /^[a-z][a-z0-9_]{4,24}$/;
+export const TEXT_BASE_PATTERN_5 = /^[\u4e00-\u9fa5a-zA-Z0-9]+$/;
+export const TEXT_BASE_PATTERN_6 = /^[\u4e00-\u9fa5a-zA-Z0-9.]+$/;
 export const _TokenPattern = /^[a-z][a-z0-9_]{4,24}$/;
 export const _ColumnNamePattern = /^[a-zA-Z][a-zA-Z0-9_]*$/;
 
