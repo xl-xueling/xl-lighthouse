73dfb11653e06f6859bb504286fa07b1571ce5fc
xl-xueling <better_xueling@126.com>
2025-07-24 16:11:53 +0800

    feat:add drag-and-drop custom view layout for BI dashboard.

--- lighthouse-insights/src/main/java/com/dtstep/lighthouse/insights/dto/ResourceParam.java
+++ lighthouse-insights/src/main/java/com/dtstep/lighthouse/insights/dto/ResourceParam.java
@@ -2,7 +2,7 @@ package com.dtstep.lighthouse.insights.dto;
 
 import com.dtstep.lighthouse.common.enums.ResourceTypeEnum;
 
-public class ViewForPublishRequestParam {
+public class ResourceParam {
 
     private Integer resourceId;
 
