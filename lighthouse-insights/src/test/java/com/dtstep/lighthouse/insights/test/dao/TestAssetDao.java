b78fe4a345d50d98a4fe600b1ee39a7edec73aa4
xl-xueling <better_xueling@126.com>
2025-08-01 18:45:52 +0800

    feat:add drag-and-drop custom view layout for BI dashboard.

--- lighthouse-insights/src/test/java/com/dtstep/lighthouse/insights/test/dao/TestAssetDao.java
+++ lighthouse-insights/src/test/java/com/dtstep/lighthouse/insights/test/dao/TestAssetDao.java
@@ -3,6 +3,8 @@ package com.dtstep.lighthouse.insights.test.dao;
 import com.dtstep.lighthouse.common.enums.AssetTypeEnum;
 import com.dtstep.lighthouse.common.enums.PrivateTypeEnum;
 import com.dtstep.lighthouse.common.modal.Asset;
+import com.dtstep.lighthouse.common.modal.AssetQueryParam;
+import com.dtstep.lighthouse.common.util.JsonUtil;
 import com.dtstep.lighthouse.insights.LightHouseInsightsApplication;
 import com.dtstep.lighthouse.insights.dao.AssetDao;
 import com.dtstep.lighthouse.insights.test.listener.SpringTestExecutionListener;
@@ -12,8 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringRunner;
-
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = LightHouseInsightsApplication.class,properties = {"spring.config.location=classpath:lighthouse-insights.yml"})
@@ -33,5 +35,14 @@ public class TestAssetDao {
         asset.setUpdateTime(LocalDateTime.now());
         asset.setPath("/uploads");
         assetDao.insert(asset);
+    }
+
+    @Test
+    public void testQuery() throws Exception {
+        AssetQueryParam queryParam = new AssetQueryParam();
+        queryParam.setAssetType(AssetTypeEnum.HEADER);
+        queryParam.setUserId(1);
+        List<Asset> assetList = assetDao.queryList(queryParam);
+        System.out.println("assetList is:" + JsonUtil.toJSONString(assetList));
     }
 }
