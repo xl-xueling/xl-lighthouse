14ed88afdcd4921e6713c0e77aea392bf3d68f57
xl-xueling <better_xueling@126.com>
2025-12-06 10:58:36 +0800

    feat:docker deployment support.

--- docker-support/scripts/restart.sh
+++ docker-support/scripts/restart.sh
@@ -3,16 +3,10 @@
 current_language=$(echo $LANG | cut -d_ -f1)
 
 if [[ "$current_language" == "zh" ]]; then
-  MESSAGE_RESTART="您即将执行一键重启，系统将重启所有容器。"
-  MESSAGE_INPUT_PROMPT="请输入 'yes' 继续重启: "
-  MESSAGE_ABORT="重启已被中止。"
   MESSAGE_RESTART_DOCKER="开始重启 Docker 环境..."
   MESSAGE_RESTART_CONTAINERS="重启容器..."
   MESSAGE_RESTART_DONE="重启完成！所有相关容器已重启。"
 else
-  MESSAGE_RESTART="You are about to perform a one-click restart. The system will restart all containers."
-  MESSAGE_INPUT_PROMPT="Please enter 'yes' to continue with restarting: "
-  MESSAGE_ABORT="Restart has been aborted."
   MESSAGE_RESTART_DOCKER="Restarting Docker environment..."
   MESSAGE_RESTART_CONTAINERS="Restarting containers..."
   MESSAGE_RESTART_DONE="Restart completed! All related containers have been restarted."
--- docker-support/scripts/start.sh
+++ docker-support/scripts/start.sh
@@ -3,16 +3,10 @@
 current_language=$(echo $LANG | cut -d_ -f1)
 
 if [[ "$current_language" == "zh" ]]; then
-  MESSAGE_START="您即将执行一键启动，系统将启动所有容器。"
-  MESSAGE_INPUT_PROMPT="请输入 'yes' 继续启动: "
-  MESSAGE_ABORT="启动已被中止。"
   MESSAGE_START_DOCKER="开始启动 Docker 环境..."
   MESSAGE_START_CONTAINERS="启动容器..."
   MESSAGE_START_DONE="启动完成！所有相关容器已启动。"
 else
-  MESSAGE_START="You are about to perform a one-click start. The system will start all containers."
-  MESSAGE_INPUT_PROMPT="Please enter 'yes' to continue with starting: "
-  MESSAGE_ABORT="Start has been aborted."
   MESSAGE_START_DOCKER="Starting Docker environment..."
   MESSAGE_START_CONTAINERS="Starting containers..."
   MESSAGE_START_DONE="Start completed! All related containers have been started."
